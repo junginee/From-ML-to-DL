d14014156ac0c3458249f2558e6eba5890b1ed1e
junginee <107666154+junginee@users.noreply.github.com>
2022-07-03 23:10:43 +0900

    07/01

---
+++ keras/keras14_6_activation_kaggle_bike.py
@@ -0,0 +1,132 @@
+#### 과제 2 ####
+# activation : sigmoid, relu, linear 넣고 돌리기
+# metrics 추가
+# EarlyStopping 넣고
+# 성능 비교
+# 감상문, 느낀점 2줄이상!!!
+
+import numpy as np
+import pandas as pd
+from sqlalchemy import true #pandas : 엑셀땡겨올때 사용
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense
+from keras.layers.recurrent import LSTM, SimpleRNN
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_squared_error
+import datetime as dt
+
+#1. 데이터
+path = './_data/kaggle_bike/'
+train_set = pd.read_csv(path + 'train.csv') # + 명령어는 문자를 앞문자와 더해줌  index_col=n n번째 컬럼을 인덱스로 인식
+            
+test_set = pd.read_csv(path + 'test.csv') # 예측에서 쓸거임        
+
+'''                        
+print(train_set)
+print(train_set.shape) # (10886, 12)
+                  
+print(test_set)
+print(test_set.shape) # (6493, 9)
+print(test_set.info()) # (715, 9)
+print(train_set.columns)
+print(train_set.info()) # info 정보출력
+print(train_set.describe()) # describe 평균치, 중간값, 최소값 등등 출력
+'''
+
+
+######## 년, 월 ,일 ,시간 분리 ############
+
+train_set["hour"] = [t.hour for t in pd.DatetimeIndex(train_set.datetime)]
+train_set["day"] = [t.dayofweek for t in pd.DatetimeIndex(train_set.datetime)]
+train_set["month"] = [t.month for t in pd.DatetimeIndex(train_set.datetime)]
+train_set['year'] = [t.year for t in pd.DatetimeIndex(train_set.datetime)]
+train_set['year'] = train_set['year'].map({2011:0, 2012:1})
+
+test_set["hour"] = [t.hour for t in pd.DatetimeIndex(test_set.datetime)]
+test_set["day"] = [t.dayofweek for t in pd.DatetimeIndex(test_set.datetime)]
+test_set["month"] = [t.month for t in pd.DatetimeIndex(test_set.datetime)]
+test_set['year'] = [t.year for t in pd.DatetimeIndex(test_set.datetime)]
+test_set['year'] = test_set['year'].map({2011:0, 2012:1})
+
+train_set.drop('datetime',axis=1,inplace=True) # 트레인 세트에서 데이트타임 드랍
+train_set.drop('casual',axis=1,inplace=True) # 트레인 세트에서 캐주얼 레지스터드 드랍
+train_set.drop('registered',axis=1,inplace=True)
+
+test_set.drop('datetime',axis=1,inplace=True) # 트레인 세트에서 데이트타임 드랍
+
+print(train_set)
+print(test_set)
+##########################################
+
+
+x = train_set.drop(['count'], axis=1)  # drop 데이터에서 ''사이 값 빼기
+print(x)
+print(x.columns)
+print(x.shape) # (10886, 12)
+
+y = train_set['count'] 
+print(y)
+print(y.shape) # (10886,)
+
+x_train, x_test, y_train, y_test = train_test_split(x,y,
+                                                    train_size=0.75,
+                                                    random_state=31
+                                                    )
+
+#2. 모델구성
+model = Sequential()
+model.add(Dense(100, activation='swish', input_dim=12))
+model.add(Dense(100, activation='ReLU'))
+model.add(Dense(100, activation='ReLU'))
+model.add(Dense(100, activation='ReLU'))
+model.add(Dense(1))
+
+#3. 컴파일, 훈련
+
+from tensorflow.python.keras.callbacks import EarlyStopping
+earlyStopping = EarlyStopping(monitor='val_loss', patience=500, mode='min', verbose=1, 
+                              restore_best_weights=True)
+
+model.compile(loss='mse', optimizer='adam', metrics=['mae'])
+model.fit(x_train, y_train, epochs=800, batch_size=100, verbose=1,validation_split=0.2, callbacks=[earlyStopping])
+
+#4. 평가, 예측
+loss = model.evaluate(x, y) 
+print('loss : ', loss)
+
+y_predict = model.predict(x_test)
+
+def RMSE(a, b): 
+    return np.sqrt(mean_squared_error(a, b))
+
+rmse = RMSE(y_test, y_predict)
+
+from sklearn.metrics import r2_score
+r2 = r2_score(y_test, y_predict)
+
+print('loss : ', loss)
+print("RMSE : ", rmse)
+
+
+
+'''
+y_summit = model.predict(test_set)
+print(y_summit)
+print(y_summit.shape) # (6493, 1)
+submission_set = pd.read_csv(path + 'sampleSubmission.csv', # + 명령어는 문자를 앞문자와 더해줌
+                             index_col=0) # index_col=n n번째 컬럼을 인덱스로 인식
+print(submission_set)
+submission_set['count'] = y_summit
+print(submission_set)
+submission_set.to_csv(path + 'submission.csv', index = True)
+
+# 1. validation 적용했을때 결과
+# loss : 5376.3001253
+# RMSE : 75.09507681950794
+
+# 2. validation / EarlyStopping / activation 적용했을때 결과
+# loss: 4674.00875046865
+# RMSE : 68.9894925467759
+
+# 1 > 2 과정을 거쳐 RMSE 값이 낮게 도출되었음을 확인했다. 
+# 이는 모델의 정밀도가 조금 더 높아졌음으로 해석할 수 있다.  
