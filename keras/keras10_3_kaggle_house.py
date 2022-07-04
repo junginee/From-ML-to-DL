7a5452c91c969788faf22dbb69e62b3633e12492
junginee <107666154+junginee@users.noreply.github.com>
2022-07-04 19:24:34 +0900

    06/28

--- keras/keras10_3_kaggle_house.py
+++ keras/keras10_3_kaggle_house.py
@@ -1,27 +1,114 @@
-import numpy as np 
+# https://www.kaggle.com/competitions/house-prices-advanced-regression-techniques
+# 캐글 하우스 프라이스 문제풀이
+import numpy as np
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_squared_error
 import matplotlib.pyplot as plt
-import seaborn as sns
+from tqdm import tqdm_notebook
 
-# 1. 데이터
-train_df = pd.read_csv('house_train.csv')
-test_df = pd.read_csv('house_test.csv')
+#1. 데이터
+path = './_data/kaggle_house/' # 경로 = .현재폴더 /하단
+train_set = pd.read_csv(path + 'train.csv', # train.csv 의 데이터가 train set에 들어가게 됨
+                        index_col=0) # 0번째 컬럼은 인덱스로 지정하는 명령
+#print(train_set)
+#print(train_set.shape) # (1460, 80) 원래 열이 81개지만, id를 인덱스로 제외하여 80개
 
-train_df.head()
-# set index       
-train_df.set_index('Id', inplace=True)  #train 데이터 1460
-test_df.set_index('Id', inplace=True)   #test 데이터 (1459,81)
-len_train_df = len(train_df)
-len_test_df = len(test_df)
+test_set = pd.read_csv(path + 'test.csv',
+                       index_col=0)
 
-corrmat = train_df.corr()
-top_corr_features = corrmat.index[abs(corrmat["SalePrice"])>=0.3]
-top_corr_features
+drop_cols = ['Alley', 'PoolQC', 'Fence', 'MiscFeature'] # Columns with more than 70% of missing values
+test_set.drop(drop_cols, axis = 1, inplace =True)
 
-# heatmap
-plt.figure(figsize=(13,10))
-g = sns.heatmap(train_df[top_corr_features].corr(),annot=True,cmap="RdYlGn")
+sample_submission = pd.read_csv(path + 'sample_submission.csv',
+                       index_col=0)
+#print(test_set)
+#print(test_set.shape) # (1459, 79) # 예측 과정에서 쓰일 예정
 
-# split y_label
-train_y_label = train_df['SalePrice'] 	# target 값을 미리 분리하였음.
-train_df.drop(['SalePrice'], axis=1, inplace=True)
+train_set.drop(drop_cols, axis = 1, inplace =True)
+cols = ['MSZoning', 'Street','LandContour','Neighborhood','Condition1','Condition2',
+                'RoofStyle','RoofMatl','Exterior1st','Exterior2nd','MasVnrType','Foundation',
+                'Heating','GarageType','SaleType','SaleCondition','ExterQual','ExterCond','BsmtQual','BsmtCond','BsmtExposure','BsmtFinType1',
+                'BsmtFinType2','HeatingQC','CentralAir','Electrical','KitchenQual','Functional',
+                'FireplaceQu','GarageFinish','GarageQual','GarageCond','PavedDrive','LotShape',
+                'Utilities','LandSlope','BldgType','HouseStyle','LotConfig']
+
+for col in tqdm_notebook(cols):
+    le = LabelEncoder()
+    train_set[col]=le.fit_transform(train_set[col])
+    test_set[col]=le.fit_transform(test_set[col])
+
+#print(train_set.columns)
+#print(train_set.info()) # 각 컬럼에 대한 디테일한 내용 출력 / null값(중간에 빠진 값) '결측치'
+#print(train_set.describe())
+
+#### 결측치 처리 1. 제거 ####
+print(train_set.isnull().sum()) # 각 컬럼당 null의 갯수 확인가능
+train_set = train_set.fillna(train_set.mean()) # nan 값을 채우거나(fillna) 행별로 모두 삭제(dropna)
+print(train_set.isnull().sum())
+print(train_set.shape) # (1460, 80) 데이터가 얼마나 삭제된 것인지 확인가능(1460-1460=0)
+ 
+
+test_set = test_set.fillna(test_set.mean())
+
+
+x = train_set.drop(['SalePrice'], axis=1) # axis는 'count'가 컬럼이라는 것을 명시하기 위해
+print(x)
+print(x.columns)
+print(x.shape) # (1460, 79)
+
+y = train_set['SalePrice']
+print(y)
+print(y.shape) # (1460, )
+
+x_train, x_test, y_train, y_test = train_test_split(x, y,
+        train_size=0.75, shuffle=True, random_state=68)
+
+
+#2. 모델구성
+model = Sequential()
+model.add(Dense(24, input_dim=75))
+model.add(Dense(100, activation='swish'))
+model.add(Dense(100, activation='swish'))
+model.add(Dense(1))
+
+#3. 컴파일, 훈련
+model.compile(loss='mae', optimizer='adam')
+history = model.fit(x_train, y_train, epochs=1000, validation_split=0.2, batch_size=100)
+
+#4. 평가 예측
+loss = model.evaluate(x_test, y_test)
+print('loss :', loss)
+
+y_predict = model.predict(x_test)
+
+def RMSE(y_test, y_predict) : #(원y값, 예측y값)
+    return np.sqrt(mean_squared_error(y_test, y_predict)) # MSE에 루트를 씌워 돌려주겠다.
+
+rmse = RMSE(y_test, y_predict)
+print("RMSE : ", rmse)
+
+
+y_summit = model.predict(test_set)
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('loss')
+plt.legend(['train_set', 'test_set'], loc='upper left')
+plt.show()
+
+# print(y_summit)
+# print(y_summit.shape) # (715, 1)
+
+
+# .to_csv()를 사용하여
+# submission.csv를 완성하시오
+
+sample_submission['SalePrice'] = y_summit
+sample_submission = sample_submission.fillna(sample_submission.mean())
+sample_submission.to_csv(path + 'test04.csv', index=True)
