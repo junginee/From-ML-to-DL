87f8ebc5252f3ab41ea378570074b0328f6866ac
junginee <107666154+junginee@users.noreply.github.com>
2022-07-11 12:55:53 +0900

    07/08

--- keras/keras29_cifar100_CNN.py
+++ keras/keras29_cifar100_CNN.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.utils import to_categorical
-
+from sklearn.preprocessing import MinMaxScaler
 
 #1. 데이터
 
@@ -14,28 +14,40 @@ from tensorflow.keras.utils import to_categorical
 print(x_train.shape, y_train.shape) # (50000, 32, 32, 3) (50000, 1)
 print(x_test.shape, y_test.shape)   # (10000, 32, 32, 3) (10000, 1)
 
-# from sklearn.preprocessing import MinMaxScaler, StandardScaler, MaxAbsScaler, RobustScaler
-# scaler = StandardScaler()
-# scaler.fit(x_train)
-# scaler.transform(x_train)
-# x_train = scaler.transform(x_train)
-# x_test = scaler.transform(x_test)
+#--------------------------------------------------------------------
+# x에 대한 전처리
 
-x_train = x_train.reshape(50000, 32, 32, 3)
+scaler = MinMaxScaler() 
+x_train = x_train.reshape(50000,-1)
+x_test = x_test.reshape(10000,-1) 
+
+print(x_train.shape, x_test.shape) #(50000, 3072) (10000, 3072)
+
+scaler.fit(x_train)
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+
+x_train = x_train.reshape(50000, 32, 32, 3)  
 x_test = x_test.reshape(10000, 32, 32, 3)
-print(x_train.shape) # (50000, 32, 32, 3)
+ 
+print(x_train.shape, x_test.shape) #(50000, 32, 32, 3) (10000, 32, 32, 3)
 
-print(np.unique(y_train, return_counts=True))
+#--------------------------------------------------------------------
+# y에 대한 전처리(원핫인코딩)
 
+import numpy as np
+print(np.unique(y_train)) #0~99까지 총 100개
 
+from tensorflow.keras.utils import to_categorical
 y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
+
 
 
 #2. 모델구성
 model = Sequential()
 model.add(Conv2D(10, kernel_size=(3,3), strides=2, padding='same', input_shape=(32, 32, 3)))   
-model.add(MaxPool2D())
+model.add(MaxPooling2D())
 model.add(Dropout(0.2))
 model.add(Conv2D(5, (2,2), activation='relu')) 
 model.add(Flatten())
@@ -62,14 +74,15 @@ earlyStopping =EarlyStopping(monitor='val_loss', patience=1, mode='min', verbose
 
 hist = model.fit(x_train, y_train, epochs=1, batch_size=1, 
                 validation_split=0.2,
-                callbacks=[earlyStopping, mcp], 
+                callbacks=[earlyStopping], 
                 verbose=1)
 
 
 
 #4. 평가, 예측
 loss = model.evaluate(x_test, y_test) 
-print('loss : ', loss)
+print('loss : ', loss[0])
+print('accuracy : ', loss[1])
 
 y_predict = model.predict(x_test)
 
@@ -77,6 +90,9 @@ from sklearn.metrics import r2_score, accuracy_score
 y_predict = np.argmax(y_predict, axis= 1)
 y_predict = to_categorical(y_predict)
 
-acc = accuracy_score(y_test, y_predict)
-print('acc스코어 : ', acc)
+#acc = accuracy_score(y_test, y_predict)
+#print('acc스코어 : ', acc)
 
+
+# loss :  3.943021774291992
+# accuracy :  0.08959999680519104
