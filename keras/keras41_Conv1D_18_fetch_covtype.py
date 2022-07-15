7bf86fa257567ee73ad1d384928778ddafeef2e0
junginee <107666154+junginee@users.noreply.github.com>
2022-07-15 10:57:39 +0900

    07/14

---
+++ keras/keras41_Conv1D_18_fetch_covtype.py
@@ -0,0 +1,168 @@
+import numpy as np 
+from sklearn.datasets import fetch_covtype
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense, LSTM, Dropout,Flatten, Conv1D
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score,accuracy_score
+from tensorflow.python.keras.callbacks import EarlyStopping
+import tensorflow as tf
+from sklearn.preprocessing import MinMaxScaler,StandardScaler,MaxAbsScaler,RobustScaler
+
+import pandas as pd
+
+#1.데이터 
+datasets = fetch_covtype()
+x= datasets.data
+y= datasets.target
+
+print(x.shape, y.shape) #(581012, 54) (581012, )
+print(np.unique(y,return_counts=True)) #(array[1 2 3 4 5 6 7],array[211840, 283301,  35754,   2747,   9493,  17367,  20510] )
+
+
+
+#사이킷런 원핫인코더
+from sklearn.preprocessing import OneHotEncoder
+one = OneHotEncoder(categories='auto',sparse= False)#False로 할 경우 넘파이 배열로 반환된다.
+y = y.reshape(-1,1)
+one.fit(y)
+y = one.transform(y)
+
+print(y)
+print(y.shape)
+
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.3,shuffle=True,
+                                                     random_state=58)
+
+scaler = RobustScaler()
+scaler.fit(x_train)
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test) 
+
+x_train = x_train.reshape(406708,27,2)
+x_test = x_test.reshape(174304,27,2)
+print(x_train.shape, x_test.shape) #(406708, 27, 2) (174304, 27, 2)
+
+#2.모델
+model = Sequential()
+model.add(LSTM(40, input_shape=(27,2)))
+model.add(Dense(30,activation ='relu'))
+model.add(Dense(30,activation ='relu'))
+model.add(Dense(30,activation ='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(30,activation ='relu'))
+model.add(Dense(7,activation ='softmax'))
+
+
+#3.컴파일,훈련
+model.compile(loss= 'categorical_crossentropy', optimizer ='adam', metrics='accuracy') 
+
+
+earlyStopping= EarlyStopping(monitor='val_loss',patience=10,mode='min',restore_best_weights=True,verbose=1)
+
+model.fit(x_train, y_train, epochs=50, batch_size=100,validation_split=0.2,callbacks=earlyStopping, verbose=1) 
+
+
+
+#4.평가,예측
+
+loss = model.evaluate(x_test,y_test)
+print('loss: ', round(loss[0],4))
+
+
+y_predict = model.predict(x_test) 
+
+y_predict = y_predict.argmax(axis=1) 
+
+y_test = y_test.argmax(axis=1) 
+acc = accuracy_score(y_test,y_predict)
+print('acc스코어: ', round(acc,4))
+
+import numpy as np 
+from sklearn.datasets import fetch_covtype
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense, LSTM, Dropout
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score,accuracy_score
+from tensorflow.python.keras.callbacks import EarlyStopping
+import tensorflow as tf
+from sklearn.preprocessing import MinMaxScaler,StandardScaler,MaxAbsScaler,RobustScaler
+
+import pandas as pd
+
+#1.데이터 
+datasets = fetch_covtype()
+x= datasets.data
+y= datasets.target
+
+print(x.shape, y.shape) #(581012, 54) (581012, )
+print(np.unique(y,return_counts=True)) #(array[1 2 3 4 5 6 7],array[211840, 283301,  35754,   2747,   9493,  17367,  20510] )
+
+
+
+#사이킷런 원핫인코더
+from sklearn.preprocessing import OneHotEncoder
+one = OneHotEncoder(categories='auto',sparse= False)#False로 할 경우 넘파이 배열로 반환된다.
+y = y.reshape(-1,1)
+one.fit(y)
+y = one.transform(y)
+
+print(y)
+print(y.shape)
+
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.3,shuffle=True,
+                                                     random_state=58)
+
+scaler = RobustScaler()
+scaler.fit(x_train)
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test) 
+
+x_train = x_train.reshape(406708,27,2)
+x_test = x_test.reshape(174304,27,2)
+print(x_train.shape, x_test.shape) #(406708, 27, 2) (174304, 27, 2)
+
+#2.모델
+model = Sequential()
+model.add(Conv1D(64,2, input_shape=(27, 2)))  
+model.add(Flatten()) 
+model.add(Dense(30,activation ='relu'))
+model.add(Dense(30,activation ='relu'))
+model.add(Dense(30,activation ='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(30,activation ='relu'))
+model.add(Dense(7,activation ='softmax'))
+
+
+#3.컴파일,훈련
+model.compile(loss= 'categorical_crossentropy', optimizer ='adam', metrics='accuracy') 
+
+
+earlyStopping= EarlyStopping(monitor='val_loss',patience=10,mode='min',restore_best_weights=True,verbose=1)
+
+model.fit(x_train, y_train, epochs=100, batch_size=100,validation_split=0.2,callbacks=earlyStopping, verbose=1) 
+
+
+
+#4.평가,예측
+
+loss = model.evaluate(x_test,y_test)
+print('loss: ', round(loss[0],4))
+
+
+y_predict = model.predict(x_test) 
+
+y_predict = y_predict.argmax(axis=1) 
+
+y_test = y_test.argmax(axis=1) 
+acc = accuracy_score(y_test,y_predict)
+print('acc스코어: ', round(acc,4))
+
+#================================= [LSTM]loss, accuracy ==========================#
+# loss:  0.3237
+# acc스코어:  0.8694
+#=================================================================================#
+
+#================================= [Conv1D]loss, accuracy ==========================#
+# loss :  0.4642
+# acc :  0.8038
+#=================================================================================#
---
+++ keras/keras41_Conv1D_25_cifar100.py
@@ -0,0 +1,101 @@
+#다시!!!!!!!!!!!!!!!!!
+
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense, Conv1D, Flatten, MaxPooling2D, Dropout # 이미지 작업은 2D
+from keras.datasets import mnist, cifar100
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import MinMaxScaler
+
+#1. 데이터
+
+(x_train, y_train), (x_test, y_test) = cifar100.load_data()
+
+print(x_train.shape, y_train.shape) # (50000, 32, 32, 3) (50000, 1)
+print(x_test.shape, y_test.shape)   # (10000, 32, 32, 3) (10000, 1)
+
+#--------------------------------------------------------------------
+# x에 대한 전처리
+
+scaler = MinMaxScaler() 
+x_train = x_train.reshape(50000,-1)
+x_test = x_test.reshape(10000,-1) 
+
+print(x_train.shape, x_test.shape) #(50000, 3072) (10000, 3072)
+
+scaler.fit(x_train)
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+
+
+x_train = x_train.reshape(50000,96,32)
+x_test = x_test.reshape(10000,96,32) 
+
+
+print(x_train.shape, x_test.shape) #(50000, 96, 32) (10000, 96, 32)
+
+#--------------------------------------------------------------------
+# y에 대한 전처리(원핫인코딩)
+
+import numpy as np
+print(np.unique(y_train)) #0~99까지 총 100개
+
+from tensorflow.keras.utils import to_categorical
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+
+
+#2. 모델구성
+model = Sequential()
+model.add(Conv1D(32, 10,activation='relu', input_shape=(96,32)))
+model.add(Flatten())
+model.add(Dense(16, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(100, activation='softmax'))
+# model.summary()
+
+
+
+#3. 컴파일, 훈련
+model.compile(loss='categorical_crossentropy', optimizer='adam',
+              metrics=['accuracy'])
+
+from tensorflow.python.keras.callbacks import EarlyStopping, ModelCheckpoint
+
+
+earlyStopping =EarlyStopping(monitor='val_loss', patience=1, mode='min', verbose=1, 
+                             restore_best_weights=True) 
+
+
+
+hist = model.fit(x_train, y_train, epochs=100, batch_size=5000, 
+                validation_split=0.2,
+                callbacks=[earlyStopping], 
+                verbose=1)
+
+
+
+#4. 평가, 예측
+loss = model.evaluate(x_test, y_test) 
+print('loss : ', loss[0])
+print('accuracy : ', loss[1])
+
+y_predict = model.predict(x_test)
+
+from sklearn.metrics import r2_score, accuracy_score
+y_predict = np.argmax(y_predict, axis= 1)
+y_predict = to_categorical(y_predict)
+
+#acc = accuracy_score(y_test, y_predict)
+#print('acc스코어 : ', acc)
+
+#[LSTM]
+# loss :  4.607076644897461
+# accuracy :  0.009999999776482582
+
+# [Conv1D]
+# loss :  3.907496452331543                                 
+# accuracy :  0.10920000076293945
