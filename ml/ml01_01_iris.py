db4be48a76c1e53fad0c5b6db06cb06d5dc4f87a
junginee <107666154+junginee@users.noreply.github.com>
2022-08-13 00:53:12 +0900

    Update ml01_01_iris.py

--- ml/ml01_01_iris.py
+++ ml/ml01_01_iris.py
@@ -5,7 +5,7 @@ from sklearn.svm import LinearSVC
 from sklearn.metrics import accuracy_score 
 import tensorflow as tf                      
 tf.random.set_seed(66)       
-                               
+                                      
 #1. 데이터                           
 datasets = load_iris()                    
                 
