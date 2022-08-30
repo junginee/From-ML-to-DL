4fde54eaa474aa9a0cca19236f4fc2f48325acaa
junginee <107666154+junginee@users.noreply.github.com>
2022-08-30 13:16:06 +0900

    Update ml03_04_fetchcovtype.py

--- ml/ml03_04_fetchcovtype.py
+++ ml/ml03_04_fetchcovtype.py
@@ -11,21 +11,11 @@ datasets = fetch_covtype()
 x = datasets.data
 y= datasets.target
 
-print(datasets.feature_names)
-print(datasets.DESCR)
-print(x.shape, y.shape) #(581012, 54) (581012,)
-print(np.unique(y, return_counts = True)) # y :[1 2 3 4 5 6 7]  / return_counts :[211840, 283301,  35754,   2747,   9493,  17367,  20510]
-
-# import pandas as pd
-# y = pd.get_dummies(y)
-# print(y)
-
 x_train, x_test, y_train, y_test = train_test_split( x, y, train_size = 0.8, shuffle=True, random_state=68 )
-
 
 #2. 모델구성
 from sklearn.svm import LinearSVC,SVC
-from sklearn.linear_model import Perceptron, LogisticRegression  #LogisicRegression 분류
+from sklearn.linear_model import Perceptron, LogisticRegression 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn. ensemble import RandomForestClassifier
@@ -41,9 +31,6 @@ for i in model:
 
 
     #4. 평가, 예측
-
     result = model.score(x_test,y_test)   
-
     y_predict = model.predict(x_test)
-
     print(f"{i} : ", round(result,4))
