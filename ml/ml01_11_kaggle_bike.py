2e6540820bd134e78bd6f7692eef0b59b495c9b5
junginee <107666154+junginee@users.noreply.github.com>
2022-08-30 12:49:33 +0900

    Update ml01_11_kaggle_bike.py

--- ml/ml01_11_kaggle_bike.py
+++ ml/ml01_11_kaggle_bike.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import pandas as pd
 from sqlalchemy import true
@@ -10,9 +9,9 @@ import datetime as dt
 
 #1. 데이터
 path = './_data/kaggle_bike/'
-train_set = pd.read_csv(path + 'train.csv') # + 명령어는 문자를 앞문자와 더해줌  index_col=n n번째 컬럼을 인덱스로 인식
+train_set = pd.read_csv(path + 'train.csv')
             
-test_set = pd.read_csv(path + 'test.csv') # 예측에서 쓸거임       
+test_set = pd.read_csv(path + 'test.csv') 
 
 ######## 년, 월 ,일 ,시간 분리 ############
 
@@ -28,10 +27,10 @@ test_set["month"] = [t.month for t in pd.DatetimeIndex(test_set.datetime)]
 test_set['year'] = [t.year for t in pd.DatetimeIndex(test_set.datetime)]
 test_set['year'] = test_set['year'].map({2011:0, 2012:1})
 
-train_set.drop('datetime',axis=1,inplace=True) # 트레인 세트에서 데이트타임 드랍
-train_set.drop('casual',axis=1,inplace=True) # 트레인 세트에서 캐주얼 레지스터드 드랍
+train_set.drop('datetime',axis=1,inplace=True) 
+train_set.drop('casual',axis=1,inplace=True) 
 train_set.drop('registered',axis=1,inplace=True)
-test_set.drop('datetime',axis=1,inplace=True) # 트레인 세트에서 데이트타임 드랍
+test_set.drop('datetime',axis=1,inplace=True) 
 
 x = train_set.drop(['count'], axis=1)  
 print(x.shape) # (10886, 12)
@@ -49,11 +48,6 @@ scaler.fit(x_train)
 x_train = scaler.transform(x_train) 
 x_test = scaler.transform(x_test)
 test_set = scaler.transform(test_set)
-print(np.min(x_train))  # 0.0
-print(np.max(x_train))  # 1.0
-
-print(np.min(x_test))  # 1.0
-print(np.max(x_test))  # 1.0
 
 #2. 모델구성
 model = LinearSVR()                 
@@ -62,16 +56,13 @@ model = LinearSVR()
 model.fit(x_train, y_train)
 
 #4. 평가, 예측
-print("캐글바이크")
-
 results = model.score(x_test, y_test)
 print("결과 : ", round(results,3)) 
 
 y_predict = model.predict(x_test) 
 from sklearn.metrics import r2_score
-r2 = r2_score(y_test, y_predict) #R2결정계수(성능평가지표)
+r2 = r2_score(y_test, y_predict) 
 print('r2스코어 : ' , round(r2,3)) 
 
-# 캐글바이크
 # 결과 :  0.32
 # r2스코어 :  0.32
