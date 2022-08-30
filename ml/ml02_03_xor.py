343b4d5dcc288dac4dacd2d7b3a37264a50b79d7
junginee <107666154+junginee@users.noreply.github.com>
2022-08-30 12:54:31 +0900

    Update ml02_03_xor.py

--- ml/ml02_03_xor.py
+++ ml/ml02_03_xor.py
@@ -3,8 +3,8 @@ from sklearn.svm import LinearSVC
 from sklearn.metrics import accuracy_score
 
 #1.데이터
-x_data = [[0,0],[0,1],[1,0],[1,1]] #(4,2)
-y_data = [0,1,1,0] #(4,)
+x_data = [[0,0],[0,1],[1,0],[1,1]]
+y_data = [0,1,1,0]
 
 #2.모델
 model = LinearSVC()
