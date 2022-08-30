f8aefb5f20f8c0b849c61dedc748c5ca4ba43b37
junginee <107666154+junginee@users.noreply.github.com>
2022-08-30 14:30:41 +0900

    Update ml03_06_kaggle_titanic.py

--- ml/ml03_06_kaggle_titanic.py
+++ ml/ml03_06_kaggle_titanic.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import pandas as pd 
 from sklearn.preprocessing import RobustScaler
@@ -7,18 +6,10 @@ from sklearn.model_selection import train_test_split
 
 #1.데이터
 
-path = './_data/kaggle_titanic/' # ".은 현재 폴더"
-train_set = pd.read_csv(path + 'train.csv',
-                        index_col=0)
-test_set = pd.read_csv(path + 'test.csv', #예측에서 쓸거야!!
-                       index_col=0)
-
-# print(test_set) 
-# print(train_set.isnull().sum()) 
-
+path = './_data/kaggle_titanic/' 
+train_set = pd.read_csv(path + 'train.csv',index_col=0)
+test_set = pd.read_csv(path + 'test.csv',index_col=0)
 train_set = train_set.fillna(train_set.median())
-
-# print(test_set.isnull().sum())
 
 
 drop_cols = ['Cabin']
@@ -26,12 +17,6 @@ train_set.drop(drop_cols, axis = 1, inplace =True)
 test_set = test_set.fillna(test_set.mean())
 train_set['Embarked'].fillna('S')
 train_set = train_set.fillna(train_set.mean())
-
-print("===============================")
-
-
-print(train_set) 
-print(train_set.isnull().sum())
 
 test_set.drop(drop_cols, axis = 1, inplace =True)
 cols = ['Name','Sex','Ticket','Embarked']
@@ -42,14 +27,11 @@ for col in tqdm_notebook(cols):
     train_set[col]=le.fit_transform(train_set[col])
     test_set[col]=le.fit_transform(test_set[col])
     
-x = train_set.drop(['Survived'],axis=1) #axis는 컬럼 
-print(x) #(891, 9)
-
+x = train_set.drop(['Survived'],axis=1) 
 y = train_set['Survived']
-print(y.shape) #(891,)
 
-gender_submission = pd.read_csv(path + 'gender_submission.csv',#예측에서 쓸거야!!
-                       index_col=0)
+
+gender_submission = pd.read_csv(path + 'gender_submission.csv', index_col=0)
 
 
 x_train, x_test, y_train, y_test = train_test_split(x,y, train_size=0.91,shuffle=True ,random_state=100)
@@ -59,10 +41,6 @@ scaler.fit(x_train)
 x_train = scaler.transform(x_train) 
 x_test = scaler.transform(x_test)
 test_set = scaler.transform(test_set)
-print(np.min(x_train))  # 0.0
-print(np.max(x_train))  # 1.0
-print(np.min(x_test))  # 1.0
-print(np.max(x_test))  # 1.0
 
 
 #2. 모델구성
@@ -83,17 +61,6 @@ for i in model:
 
 
     #4. 평가, 예측
-
     result = model.score(x_test,y_test)   
-
     y_predict = model.predict(x_test)
-
-    print(f"{i} : ", round(result,4))
-  
-    
-# Perceptron() :  0.7531   
-# SVC() :  0.7778
-# KNeighborsClassifier() :  0.8148
-# LogisticRegression() :  0.7654
-# DecisionTreeClassifier() :  0.8025
-# RandomForestClassifier() :  0.8395
+    print(f"{i} : ", round(result,4))   
