060c6cfb4847afcaf55860804489ad5b4b377565
junginee <107666154+junginee@users.noreply.github.com>
2022-08-30 12:16:01 +0900

    Update ml01_01_iris.py

--- ml/ml01_01_iris.py
+++ ml/ml01_01_iris.py
@@ -10,29 +10,21 @@ tf.random.set_seed(66)
 datasets = load_iris()                                                       
                                
 x = datasets['data']                               
-y = datasets.target                  
-          
+y = datasets.target             
+     
 
 print(x.shape, y.shape) 
 print("y의 라벨값(y의 고유값)", np.unique(y)) #y의 라벨값(y의 고유값) [0 1 2]
-
-# from tensorflow.keras.utils import to_categorical 
-# y = to_categorical(y)
-
 x_train, x_test, y_train, y_test = train_test_split( x, y, train_size = 0.8, shuffle=True, random_state=68 )
 
 #2. 모델구성
-
 model = LinearSVC() #linearsvc 모델은 원핫인코딩 사용x
 
 #3. 컴파일, 훈련
-
 model.fit(x_train, y_train)
 
 #4. 평가, 예측
-
 results = model.score(x_test, y_test)
-
 print("결과 acc : ", round(results,3)) 
 
 from sklearn.metrics import r2_score, accuracy_score  
