6d9b9547a1247727219a916afa57db427d87d4e8
junginee <107666154+junginee@users.noreply.github.com>
2022-08-30 15:35:28 +0900

    Update ml03_11_kaggle_bike.py

--- ml/ml03_11_kaggle_bike.py
+++ ml/ml03_11_kaggle_bike.py
@@ -10,11 +10,10 @@ import datetime as dt
 
 #1. 데이터
 path = './_data/kaggle_bike/'
-train_set = pd.read_csv(path + 'train.csv') # + 명령어는 문자를 앞문자와 더해줌  index_col=n n번째 컬럼을 인덱스로 인식
-            
-test_set = pd.read_csv(path + 'test.csv') # 예측에서 쓸거임       
+train_set = pd.read_csv(path + 'train.csv')             
+test_set = pd.read_csv(path + 'test.csv') 
 
-######## 년, 월 ,일 ,시간 분리 ############
+######## Year, month, day, time separation ############
 
 train_set["hour"] = [t.hour for t in pd.DatetimeIndex(train_set.datetime)]
 train_set["day"] = [t.dayofweek for t in pd.DatetimeIndex(train_set.datetime)]
@@ -28,16 +27,13 @@ test_set["month"] = [t.month for t in pd.DatetimeIndex(test_set.datetime)]
 test_set['year'] = [t.year for t in pd.DatetimeIndex(test_set.datetime)]
 test_set['year'] = test_set['year'].map({2011:0, 2012:1})
 
-train_set.drop('datetime',axis=1,inplace=True) # 트레인 세트에서 데이트타임 드랍
-train_set.drop('casual',axis=1,inplace=True) # 트레인 세트에서 캐주얼 레지스터드 드랍
+train_set.drop('datetime',axis=1,inplace=True)
+train_set.drop('casual',axis=1,inplace=True) 
 train_set.drop('registered',axis=1,inplace=True)
-test_set.drop('datetime',axis=1,inplace=True) # 트레인 세트에서 데이트타임 드랍
+test_set.drop('datetime',axis=1,inplace=True)
 
 x = train_set.drop(['count'], axis=1)  
-print(x.shape) # (10886, 12)
-
 y = train_set['count'] 
-print(y.shape) # (10886,)
 
 x_train, x_test, y_train, y_test = train_test_split(x,y,
                                                     train_size=0.75,
@@ -49,20 +45,15 @@ scaler.fit(x_train)
 x_train = scaler.transform(x_train) 
 x_test = scaler.transform(x_test)
 test_set = scaler.transform(test_set)
-print(np.min(x_train))  # 0.0
-print(np.max(x_train))  # 1.0
-
-print(np.min(x_test))  # 1.0
-print(np.max(x_test))  # 1.0
 
 
 #2. 모델구성
-from sklearn.svm import LinearSVC,SVC
+from sklearn.svm import LinearSVC
 from sklearn.linear_model import Perceptron
-from sklearn.linear_model import LogisticRegression, LinearRegression  #LogisicRegression 분류
-from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
-from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
-from sklearn. ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.linear_model import LinearRegression  
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn. ensemble import RandomForestRegressor
 
 model = Perceptron(),LinearSVC(),LinearRegression(),KNeighborsRegressor(),DecisionTreeRegressor(),RandomForestRegressor()
 
@@ -76,11 +67,8 @@ for i in model:
 
 
     #4. 평가, 예측
-
-    result = model.score(x_test,y_test)   
-
+    result = model.score(x_test,y_test)  
     y_predict = model.predict(x_test)
-
     print(f"{i} : ", round(result,4))
     
 # Perceptron() :  0.0051    
