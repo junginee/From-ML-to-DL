f072acd30ce806274256d152262b02f325c4e795
junginee <107666154+junginee@users.noreply.github.com>
2022-08-30 12:55:23 +0900

    Update ml02_05_xor_keras1.py

--- ml/ml02_05_xor_keras1.py
+++ ml/ml02_05_xor_keras1.py
@@ -5,8 +5,8 @@ from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense
 
 #1.데이터
-x_data = [[0,0],[0,1],[1,0],[1,1]] #(4,2)
-y_data = [0,1,1,0] #(4,)
+x_data = [[0,0],[0,1],[1,0],[1,1]]
+y_data = [0,1,1,0] 
 
 #2.모델
 # model = LinearSVC()
@@ -16,9 +16,7 @@ model.add(Dense(1, input_dim = 2, activation = 'sigmoid'))
 
 #3.훈련
 # model.fit(x_data, y_data)
-
-model.compile(loss = 'binary_crossentropy', optimizer = 'adam'
-              ,metrics = ['acc'])
+model.compile(loss = 'binary_crossentropy', optimizer = 'adam',metrics = ['acc'])
 model.fit(x_data, y_data, batch_size = 1, epochs=100)
 
 #4.평가, 예측
