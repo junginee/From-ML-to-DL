f592514aafda7abd0625e9461a2cc0368abb363a
junginee <107666154+junginee@users.noreply.github.com>
2022-08-30 13:16:51 +0900

    Update ml03_05_digits.py

--- ml/ml03_05_digits.py
+++ ml/ml03_05_digits.py
@@ -2,17 +2,14 @@ import numpy as np
 from sklearn.preprocessing import RobustScaler
 from sklearn.model_selection import train_test_split
 from sklearn. datasets import load_digits
+import tensorflow as tf
+tf.random.set_seed(66)
+
 
 #1. 데이터
 datasets = load_digits()
 x = datasets.data
 y= datasets.target
-
-print(x.shape, y.shape)
-print(np.unique(y)) 
-
-import tensorflow as tf
-tf.random.set_seed(66)
 
 x_train, x_test, y_train, y_test = train_test_split( x, y, train_size = 0.8, shuffle=True, random_state=68 )
 
@@ -20,9 +17,6 @@ scaler = RobustScaler()
 scaler.fit(x_train)
 x_train = scaler.transform(x_train) 
 x_test = scaler.transform(x_test)
-print(np.min(x_train)) 
-print(np.max(x_train))
-
 
 #2. 모델구성
 from sklearn.svm import LinearSVC,SVC
@@ -42,16 +36,6 @@ for i in model:
 
 
     #4. 평가, 예측
-
-    result = model.score(x_test,y_test)   
-
+    result = model.score(x_test,y_test)
     y_predict = model.predict(x_test)
-
     print(f"{i} : ", round(result,4))
-
-# Perceptron() :  0.9528    
-# SVC() :  0.975
-# KNeighborsClassifier() :  0.9056
-# LogisticRegression() :  0.9722
-# DecisionTreeClassifier() :  0.8278
-# RandomForestClassifier() :  0.975    
