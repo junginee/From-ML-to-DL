85d34b885027d02f256f1f728f2142d154759047
junginee <107666154+junginee@users.noreply.github.com>
2022-08-30 15:32:02 +0900

    Update ml03_09_california.py

--- ml/ml03_09_california.py
+++ ml/ml03_09_california.py
@@ -2,29 +2,25 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import fetch_california_housing
 from sklearn.preprocessing import RobustScaler
-from sklearn.svm import LinearSVC, LinearSVR
 
 datasets = fetch_california_housing() 
 x = datasets.data 
 y = datasets.target 
 x_train, x_test, y_train, y_test = train_test_split(x,y, train_size=0.7, shuffle=True, random_state=5)
 
-
 scaler = RobustScaler()
 scaler.fit(x_train)
 x_train = scaler.transform(x_train) 
 x_test = scaler.transform(x_test)
-print(np.min(x_train)) #0.0 
-print(np.max(x_train)) #1.0
 
 
 #2. 모델구성
-from sklearn.svm import LinearSVC,SVC
+from sklearn.svm import LinearSVC
 from sklearn.linear_model import Perceptron
-from sklearn.linear_model import LogisticRegression, LinearRegression  #LogisicRegression 분류
-from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
-from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
-from sklearn. ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn. ensemble import RandomForestRegressor
 
 model = Perceptron(),LinearSVC(),LinearRegression(),KNeighborsRegressor(),DecisionTreeRegressor(),RandomForestRegressor()
 
@@ -38,9 +34,6 @@ for i in model:
 
 
     #4. 평가, 예측
-
-    result = model.score(x_test,y_test)   
-
+    result = model.score(x_test,y_test) 
     y_predict = model.predict(x_test)
-
     print(f"{i} : ", round(result,4))
