07e38826f5ffef1afdf242c2bbabd8e13721ce44
junginee <107666154+junginee@users.noreply.github.com>
2022-08-30 12:35:33 +0900

    Update ml01_08_boston.py

--- ml/ml01_08_boston.py
+++ ml/ml01_08_boston.py
@@ -15,10 +15,8 @@ x_train, x_test, y_train, y_test = train_test_split(x, y,
                                                     test_size=0.2,
                                                     shuffle=True,
                                                     random_state=66)
-
 #2.모델구성
 model = LinearSVR()
-
 
  
 #3.컴파일,훈련
@@ -26,15 +24,12 @@ model.fit(x_train, y_train)
 
 
 #4.평가,예측
-print("보스톤")
 results = model.score(x_test, y_test)
 print("결과 : ", round(results,3)) 
 
 y_predict = model.predict(x_test) 
-r2 = r2_score(y_test, y_predict) #R2결정계수(성능평가지표)
+r2 = r2_score(y_test, y_predict)
 print('r2스코어 : ' , round(r2,3)) 
 
-# 보스톤
 # 결과 :  0.746
 # r2스코어 :  0.746
-
