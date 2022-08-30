b131ba28cc2a0a48abb36f0617a71b5acf652118
junginee <107666154+junginee@users.noreply.github.com>
2022-08-30 15:33:47 +0900

    Update ml03_10_ddarung.py

--- ml/ml03_10_ddarung.py
+++ ml/ml03_10_ddarung.py
@@ -7,32 +7,12 @@ from sklearn.svm import LinearSVR
 
 #1.데이터
 path = './_data/ddarung/'
-train_set = pd.read_csv(path + 'train.csv', index_col =0) #id는 0번째에 위치한다. #[1459 rows x 10 columns]
-
-print(train_set)
-print(train_set.shape) #(1459,10)
-
+train_set = pd.read_csv(path + 'train.csv', index_col =0)
 test_set = pd.read_csv(path + 'test.csv', index_col =0) 
-print(test_set)
-print(test_set.shape)  #(715, 9)
 
-print(train_set.columns)
-print(train_set.info())  
-print(train_set.describe())
-
-
-print(train_set.isnull().sum()) #train set에 있는 널값의 합계를 구한다.
-train_set = train_set.dropna() #결측치가 들어있는 행을 삭제한다.
-print(train_set.isnull().sum()) #결측치 제거 후 train set에 들어있는 널값의 합계를 구한다.
-x = train_set.drop(['count'], axis = 1) #x 변수에는 count 열을 제외한 나머지 컬럼을 저장한다.
-
-print(x)
-print(x.columns)
-print(x.shape) #(1459,9)
-
-y = train_set['count'] #count 컬럼만 y 변수에 저장한다.
-print(y)
-print(y.shape)
+train_set = train_set.dropna() 
+x = train_set.drop(['count'], axis = 1)
+y = train_set['count']
 
 x_train,x_test,y_train, y_test = train_test_split(x,y, train_size=0.7, shuffle=True, random_state=72)
 
@@ -41,20 +21,15 @@ scaler.fit(x_train)
 x_train = scaler.transform(x_train) 
 x_test = scaler.transform(x_test)
 test_set = scaler.transform(test_set)
-print(np.min(x_train))  # 0.0
-print(np.max(x_train))  # 1.0
-
-print(np.min(x_test))  # 1.0
-print(np.max(x_test))  # 1.0
 
 
 #2. 모델구성
-from sklearn.svm import LinearSVC,SVC
+from sklearn.svm import LinearSVC
 from sklearn.linear_model import Perceptron
-from sklearn.linear_model import LogisticRegression, LinearRegression  #LogisicRegression 분류
-from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
-from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
-from sklearn. ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn. ensemble import RandomForestRegressor
 
 model = Perceptron(),LinearSVC(),LinearRegression(),KNeighborsRegressor(),DecisionTreeRegressor(),RandomForestRegressor()
 
@@ -68,9 +43,7 @@ for i in model:
 
 
     #4. 평가, 예측
-
     result = model.score(x_test,y_test)   
-
     y_predict = model.predict(x_test)
 
     print(f"{i} : ", round(result,4))
