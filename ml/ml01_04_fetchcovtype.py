f818a9d95e96f89f81e2357af3728960d298f921
junginee <107666154+junginee@users.noreply.github.com>
2022-08-30 12:30:37 +0900

    Update ml01_04_fetchcovtype.py

--- ml/ml01_04_fetchcovtype.py
+++ ml/ml01_04_fetchcovtype.py
@@ -6,20 +6,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score 
 from sklearn.svm import LinearSVC
 
-
 #1. 데이터
 datasets = fetch_covtype()
 x = datasets.data
 y= datasets.target
 
-print(datasets.feature_names)
-print(datasets.DESCR)
-print(x.shape, y.shape) #(581012, 54) (581012,)
-print(np.unique(y, return_counts = True)) # y :[1 2 3 4 5 6 7]  / return_counts :[211840, 283301,  35754,   2747,   9493,  17367,  20510]
-
-# import pandas as pd
-# y = pd.get_dummies(y)
-# print(y)
+# print(datasets.feature_names)
+# print(datasets.DESCR)
+# print(x.shape, y.shape)
+# print(np.unique(y, return_counts = True)) 
 
 x_train, x_test, y_train, y_test = train_test_split( x, y, train_size = 0.8, shuffle=True, random_state=68 )
 
@@ -41,5 +36,3 @@ y_predict = model.predict(x_test)
 
 acc= accuracy_score(y_test, y_predict)
 print('acc스코어 : ', round(acc,3))  
-
-
