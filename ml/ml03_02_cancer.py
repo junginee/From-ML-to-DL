c6368f3dff3eb316713c93bd6bc8880c04430a5c
junginee <107666154+junginee@users.noreply.github.com>
2022-08-31 10:29:45 +0900

    Update ml03_02_cancer.py

--- ml/ml03_02_cancer.py
+++ ml/ml03_02_cancer.py
@@ -39,3 +39,12 @@ for i in model:
     result = model.score(x_test,y_test)   
     y_predict = model.predict(x_test)
     print(f"{i} : ", round(result,4))      
+    
+'''
+Perceptron() :  0.9415
+SVC() :  0.9766
+KNeighborsClassifier() :  0.9591
+LogisticRegression() :  0.9708
+DecisionTreeClassifier() :  0.9123
+RandomForestClassifier() :  0.9532    
+'''
