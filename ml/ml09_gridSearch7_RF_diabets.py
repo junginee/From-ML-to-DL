e776fe254cab94928191f8e866acbd2839c0b22c
junginee <107666154+junginee@users.noreply.github.com>
2022-08-31 11:45:10 +0900

    Update ml09_gridSearch7_RF_diabets.py

--- ml/ml09_gridSearch7_RF_diabets.py
+++ ml/ml09_gridSearch7_RF_diabets.py
@@ -6,7 +6,6 @@ from sklearn.svm import LinearSVR
 from sklearn. ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score, accuracy_score
 
-
 datasets =load_diabetes()
 x = datasets.data
 y = datasets.target
@@ -29,19 +28,14 @@ parameters = [
 
 #2. 모델구성
 from sklearn.svm import LinearSVC, SVC
-from sklearn.linear_model import Perceptron, LogisticRegression # LogisticRegression는 분류임
+from sklearn.linear_model import Perceptron, LogisticRegression 
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 
-model =  RandomForestRegressor(min_samples_leaf=3, min_samples_split=5)                        
-# model = GridSearchCV(RandomForestRegressor(), parameters, cv=kfold, verbose=1,          
-#                      refit=True, n_jobs=1)                             
-                                                                           
-                                                                          
-                                                                          
-
-
+model =  RandomForestRegressor(min_samples_leaf=3, min_samples_split=5)                      
+                                                                                                 
+                                                                       
 #3. 컴파일, 훈련
 model.fit(x_train, y_train)
 
