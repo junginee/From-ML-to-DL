4057e1922927bd58adeb1b3004ef1fe343a1a2c3
junginee <107666154+junginee@users.noreply.github.com>
2022-08-31 10:32:56 +0900

    Update ml03_04_fetchcovtype.py

--- ml/ml03_04_fetchcovtype.py
+++ ml/ml03_04_fetchcovtype.py
@@ -1,36 +1,71 @@
-import numpy as np
-import tensorflow as tf
-from sklearn. datasets import fetch_covtype 
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score 
-from sklearn.svm import LinearSVC
+from sklearn.metrics import r2_score
+from sklearn.datasets import fetch_covtype
 
 
-#1. 데이터
+# 1. 데이터
 datasets = fetch_covtype()
 x = datasets.data
-y= datasets.target
+y = datasets.target
+x_train, x_test, y_train, y_test =  train_test_split(x, y, train_size=0.9, shuffle=True, random_state=86)
 
-x_train, x_test, y_train, y_test = train_test_split( x, y, train_size = 0.8, shuffle=True, random_state=68 )
 
-#2. 모델구성
-from sklearn.svm import LinearSVC,SVC
+# 2. 모델구성
+from sklearn.svm import LinearSVC, SVC
 from sklearn.linear_model import Perceptron, LogisticRegression 
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier 
 from sklearn.tree import DecisionTreeClassifier
-from sklearn. ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier 
 
-model = Perceptron(),SVC(),KNeighborsClassifier(),LogisticRegression(),DecisionTreeClassifier(),RandomForestClassifier()
+'''
+model = LinearSVC()
+model.fit(x_train, y_train)
+result = model.score(x_test, y_test)
+print('LinearSVC acc 결과: ', result)
+# y_predict = model.predict(x_test)
+# print('ypred: ', y_predict, '\n')
 
-for i in model:    
-    model = i
-    
+model = SVC()
+model.fit(x_train, y_train)
+result = model.score(x_test, y_test)
+print('SVC acc 결과: ', result)
+# y_predict = model.predict(x_test)
+# print('ypred: ', y_predict, '\n')
 
-    #3. 컴파일, 훈련
-    model.fit(x_train, y_train)
+model = Perceptron()
+model.fit(x_train, y_train)
+result = model.score(x_test, y_test)
+print('Perceptron acc 결과: ', result)
+# y_predict = model.predict(x_test)
+# print('ypred: ', y_predict, '\n')
 
+model = LogisticRegression()
+model.fit(x_train, y_train)
+result = model.score(x_test, y_test)
+print('LogisticRegression acc 결과: ', result)
+# y_predict = model.predict(x_test)
+# print('ypred: ', y_predict, '\n')
 
-    #4. 평가, 예측
-    result = model.score(x_test,y_test)   
-    y_predict = model.predict(x_test)
-    print(f"{i} : ", round(result,4))
+model = KNeighborsClassifier()
+model.fit(x_train, y_train)
+result = model.score(x_test, y_test)
+print('KNeighborsClassifier acc 결과: ', result)
+# y_predict = model.predict(x_test)
+# print('ypred: ', y_predict, '\n')
+
+model = DecisionTreeClassifier()
+model.fit(x_train, y_train)
+result = model.score(x_test, y_test)
+print('DecisionTreeClassifier acc 결과: ', result)
+# y_predict = model.predict(x_test)
+# print('ypred: ', y_predict, '\n')
+'''
+
+model = RandomForestClassifier()
+model.fit(x_train, y_train)
+result = model.score(x_test, y_test)
+print('RandomForestClassifier acc 결과: ', result)
+# y_predict = model.predict(x_test)
+# print('ypred: ', y_predict, '\n')
