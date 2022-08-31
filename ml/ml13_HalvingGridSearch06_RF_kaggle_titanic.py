4f70f657956d1a1b251de8ae4746c1030abcd297
junginee <107666154+junginee@users.noreply.github.com>
2022-08-31 12:09:49 +0900

    Update ml13_HalvingGridSearch06_RF_kaggle_titanic.py

--- ml/ml13_HalvingGridSearch06_RF_kaggle_titanic.py
+++ ml/ml13_HalvingGridSearch06_RF_kaggle_titanic.py
@@ -1,11 +1,9 @@
 import numpy as np
 import pandas as pd 
-
-
 from sklearn.preprocessing import MaxAbsScaler, RobustScaler
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.experimental import enable_halving_search_cv  #얘가 더 위에 있어야 함
+from sklearn.experimental import enable_halving_search_cv  
 from sklearn.model_selection import KFold, cross_val_score, GridSearchCV, HalvingRandomSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
@@ -19,19 +17,13 @@ warnings.filterwarnings("ignore")
 
 #1.데이터
 
-path = './_data/kaggle_titanic/' # ".은 현재 폴더"
+path = './_data/kaggle_titanic/' 
 train_set = pd.read_csv(path + 'train.csv',
                         index_col=0)
-test_set = pd.read_csv(path + 'test.csv', #예측에서 쓸거야!!
+test_set = pd.read_csv(path + 'test.csv',
                        index_col=0)
 
-# print(test_set) 
-# print(train_set.isnull().sum()) 
-
 train_set = train_set.fillna(train_set.median())
-
-# print(test_set.isnull().sum())
-
 
 drop_cols = ['Cabin']
 train_set.drop(drop_cols, axis = 1, inplace =True)
@@ -40,10 +32,6 @@ train_set['Embarked'].fillna('S')
 train_set = train_set.fillna(train_set.mean())
 
 print("===============================")
-
-
-print(train_set) 
-print(train_set.isnull().sum())
 
 test_set.drop(drop_cols, axis = 1, inplace =True)
 cols = ['Name','Sex','Ticket','Embarked']
@@ -54,13 +42,10 @@ for col in tqdm_notebook(cols):
     train_set[col]=le.fit_transform(train_set[col])
     test_set[col]=le.fit_transform(test_set[col])
     
-x = train_set.drop(['Survived'],axis=1) #axis는 컬럼 
-print(x) #(891, 9)
-
+x = train_set.drop(['Survived'],axis=1)
 y = train_set['Survived']
-print(y.shape) #(891,)
 
-gender_submission = pd.read_csv(path + 'gender_submission.csv',#예측에서 쓸거야!!
+gender_submission = pd.read_csv(path + 'gender_submission.csv',
                        index_col=0)
 
 
