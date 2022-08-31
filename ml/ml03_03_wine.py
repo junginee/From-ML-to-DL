045d5ee0f41bd65bb0185015b2cee8159d97510d
junginee <107666154+junginee@users.noreply.github.com>
2022-08-31 10:30:41 +0900

    Update ml03_03_wine.py

--- ml/ml03_03_wine.py
+++ ml/ml03_03_wine.py
@@ -39,3 +39,12 @@ for i in model:
     result = model.score(x_test,y_test)   
     y_predict = model.predict(x_test)
     print(f"{i} : ", round(result,4))
+    
+'''    
+Perceptron() :  0.9167
+SVC() :  1.0
+KNeighborsClassifier() :  0.9444
+LogisticRegression() :  0.9167
+DecisionTreeClassifier() :  0.8889
+RandomForestClassifier() :  0.9722    
+'''
