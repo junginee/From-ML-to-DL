c65b00c2bfa439e20f4902bda98cf0a17d0439ce
junginee <107666154+junginee@users.noreply.github.com>
2022-08-31 13:05:29 +0900

    Update ml18_Pipeline_gridSearch01_iris.py

--- ml/ml18_Pipeline_gridSearch01_iris.py
+++ ml/ml18_Pipeline_gridSearch01_iris.py
@@ -1,28 +1,24 @@
-from multiprocessing import Pipe
 import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split, KFold
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from multiprocessing import Pipe
+from sklearn.svm import LinearSVC, SVC
+from sklearn. ensemble import RandomForestClassifier
+from sklearn.pipeline import make_pipeline, Pipeline
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+from sklearn.experimental import enable_halving_search_cv
+from sklearn. model_selection import HalvingGridSearchCV, HalvingRandomSearchCV
 
 #1. 데이터
 datasets = load_iris()
 x = datasets.data
 y = datasets.target
 
-
 x_train, x_test, y_train, y_test = train_test_split(x, y,
         train_size=0.8,shuffle=True, random_state=1234)
 
 
-
 #2. 모델구성
-from sklearn.svm import LinearSVC, SVC
-from sklearn. ensemble import RandomForestClassifier
-from sklearn.pipeline import make_pipeline, Pipeline
-
-
-# model = SVC()
-# model = make_pipeline(MinMaxScaler(),PCA(),RandomForestClassifier())  # piepline을 통해 순서대로 이동
 pipe = Pipeline([('minmax', MinMaxScaler()), ('RF', RandomForestClassifier())], verbose=1)                                             
 
 parameters = [
@@ -32,17 +28,9 @@ parameters = [
 
 n_splits = 5
 kfold = KFold(n_splits=n_splits, shuffle = True, random_state=66)
-# gridsearch 사용 시 
-# 언더바 2개 사용
-# 언더바 2개 앞에는 내가 위에서 지정한 문자열, 뒤에는 하이퍼파라미터 이름 기재
                                             
 #3. 훈련
-from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
-from sklearn.experimental import enable_halving_search_cv
-from sklearn. model_selection import HalvingGridSearchCV, HalvingRandomSearchCV
-
 model = GridSearchCV(pipe, parameters, cv= kfold, verbose=1)
-
 model.fit(x_train, y_train) 
 
 #4. 평가, 예측
