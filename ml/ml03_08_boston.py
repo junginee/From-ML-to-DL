afb174118563e9ddb26c5206e2d85dd3a91ed4c2
junginee <107666154+junginee@users.noreply.github.com>
2022-08-31 10:36:10 +0900

    Update ml03_08_boston.py

--- ml/ml03_08_boston.py
+++ ml/ml03_08_boston.py
@@ -1,39 +1,78 @@
-import numpy as np
-from sklearn.model_selection import train_test_split
 from sklearn.datasets import load_boston
-from sklearn.preprocessing import RobustScaler
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+import time
 
-#1.데이터
+# 1. 데이터
 datasets = load_boston()
 x = datasets.data
 y = datasets.target
-
-x_train, x_test, y_train, y_test = train_test_split(x, y, 
-                                                    test_size=0.2,
-                                                    shuffle=True,
-                                                    random_state=66)
+x_train, x_test, y_train, y_test =  train_test_split(x, y, train_size=0.8, shuffle=True, random_state=66)
 
 
 #2. 모델구성
-from sklearn.svm import LinearSVC
+from sklearn.svm import LinearSVR, SVR
 from sklearn.linear_model import Perceptron
-from sklearn.linear_model import LinearRegression  
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.tree import DecisionTreeRegressor
-from sklearn. ensemble import RandomForestRegressor
+from sklearn.linear_model import LogisticRegression, LinearRegression
+from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 
-model = Perceptron(),LinearSVC(),LinearRegression(),KNeighborsRegressor(),DecisionTreeRegressor(),RandomForestRegressor()
+model = LinearSVR()
+model.fit(x_train, y_train)
+result = model.score(x_test, y_test)
+print('LinearSVR r2 결과: ', result)
+# y_predict = model.predict(x_test)
+# print('ypred: ', y_predict, '\n')
 
+model = SVR()
+model.fit(x_train, y_train)
+result = model.score(x_test, y_test)
+print('SVR r2 결과: ', result)
+# y_predict = model.predict(x_test)
+# print('ypred: ', y_predict, '\n')
 
-for i in model:    
-    model = i
-    
+# model = Perceptron()
+# model.fit(x_train, y_train)
+# result = model.score(x_test, y_test)
+# print('Perceptron r2 결과: ', result)
+# # y_predict = model.predict(x_test)
+# # print('ypred: ', y_predict, '\n')
 
-    #3. 컴파일, 훈련
-    model.fit(x_train, y_train)
+model = LinearRegression()
+model.fit(x_train, y_train)
+result = model.score(x_test, y_test)
+print('LinearRegression r2 결과: ', result)
+# y_predict = model.predict(x_test)
+# print('ypred: ', y_predict, '\n')
 
+model = KNeighborsRegressor()
+model.fit(x_train, y_train)
+result = model.score(x_test, y_test)
+print('KNeighborsRegressor r2 결과: ', result)
+# y_predict = model.predict(x_test)
+# print('ypred: ', y_predict, '\n')
 
-    #4. 평가, 예측
-    result = model.score(x_test,y_test)   
-    y_predict = model.predict(x_test)
-    print(f"{i} : ", round(result,4))
+model = DecisionTreeRegressor()
+model.fit(x_train, y_train)
+result = model.score(x_test, y_test)
+print('DecisionTreeRegressor r2 결과: ', result)
+# y_predict = model.predict(x_test)
+# print('ypred: ', y_predict, '\n')
+
+model = RandomForestRegressor()
+model.fit(x_train, y_train)
+result = model.score(x_test, y_test)
+print('RandomForestRegressor r2 결과: ', result)
+# y_predict = model.predict(x_test)
+# print('ypred: ', y_predict, '\n')
+
+# LinearSVR r2 결과:  0.7434063515479603
+# SVR r2 결과:  0.23474677555722312
+# SVR r2 결과:  0.23474677555722312
+# LinearRegression r2 결과:  0.8111288663608656
+# KNeighborsRegressor r2 결과:  0.5900872726222293
+# DecisionTreeRegressor r2 결과:  0.7780553674479604
+# RandomForestRegressor r2 결과:  0.9204893478849648
