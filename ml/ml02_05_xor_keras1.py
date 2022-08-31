444fa4327e9fc3c9e7e467e5c1088f50c1001573
junginee <107666154+junginee@users.noreply.github.com>
2022-08-31 10:25:02 +0900

    Update ml02_05_xor_keras1.py

--- ml/ml02_05_xor_keras1.py
+++ ml/ml02_05_xor_keras1.py
@@ -1,25 +1,36 @@
 import numpy as np
 from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
+from sklearn.linear_model import Perceptron
 from sklearn.metrics import accuracy_score
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense
 
-#1.데이터
+# 1. 데이터
 x_data = [[0,0],[0,1],[1,0],[1,1]]
-y_data = [0,1,1,0] 
+y_data = [0,1,1,0] # xor: 같으면 0 다르면 1
 
-#2.모델
+# 2. 모델
+# model = LinearSVC()
+# model = Perceptron()
+# model = SVC()
 model = Sequential()
-model.add(Dense(1, input_dim = 2, activation = 'sigmoid'))
+model.add(Dense(1, input_dim=2, activation='sigmoid'))
 
-#3.훈련
-# model.fit(x_data, y_data)
-model.compile(loss = 'binary_crossentropy', optimizer = 'adam',metrics = ['acc'])
-model.fit(x_data, y_data, batch_size = 1, epochs=100)
+# 3. 훈련
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+model.fit(x_data, y_data, batch_size=1, epochs=100)
 
-#4.평가, 예측
-y_predict = model.predict(x_data)
-print(x_data, "의 예측결과 : ", y_predict)
+# 4. 평가, 예측
+y_pred = model.predict(x_data)
+print(x_data, '의 예측결과: ', y_pred)
 
-results = model.evaluate(x_data, y_data)
-print("model.evaluate :", results[1])
+result = model.evaluate(x_data, y_data)
+print('model.score: ', result[1])
+
+
+# [[0, 0], [0, 1], [1, 0], [1, 1]] 의 예측결과:  [[0.47643304]
+#  [0.57909226]
+#  [0.63553977]
+#  [0.72500855]]
+# model.score:  0.75
