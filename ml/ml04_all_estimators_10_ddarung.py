6e541df68de8e91759096166166cde8991d0674a
junginee <107666154+junginee@users.noreply.github.com>
2022-08-31 10:41:47 +0900

    Update ml04_all_estimators_10_ddarung.py

--- ml/ml04_all_estimators_10_ddarung.py
+++ ml/ml04_all_estimators_10_ddarung.py
@@ -1,72 +1,73 @@
 import numpy as np
-import pandas as pd 
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error
-from sklearn.preprocessing import StandardScaler
-from sklearn.svm import LinearSVR
+from sklearn.svm import LinearSVR # 레거시한 리니어 모델 사용
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.utils import all_estimators
-from sklearn.metrics import accuracy_score
-
+from sklearn.metrics import accuracy_score, r2_score
 import warnings
-warnings.filterwarnings("ignore")
+warnings.filterwarnings('ignore')
 
-#1.데이터
-path = './_data/ddarung/'
-train_set = pd.read_csv(path + 'train.csv', index_col =0) #id는 0번째에 위치한다. #[1459 rows x 10 columns]
-
-print(train_set)
-print(train_set.shape) #(1459,10)
-
-test_set = pd.read_csv(path + 'test.csv', index_col =0) 
-print(test_set)
-print(test_set.shape)  #(715, 9)
-
-print(train_set.columns)
-print(train_set.info())  
-print(train_set.describe())
+#1. 데이터
+path = './_data/ddarung/' 
+train_set = pd.read_csv(path + 'train.csv', 
+                        index_col=0)
 
 
-print(train_set.isnull().sum()) #train set에 있는 널값의 합계를 구한다.
-train_set = train_set.dropna() #결측치가 들어있는 행을 삭제한다.
-print(train_set.isnull().sum()) #결측치 제거 후 train set에 들어있는 널값의 합계를 구한다.
-x = train_set.drop(['count'], axis = 1) #x 변수에는 count 열을 제외한 나머지 컬럼을 저장한다.
+test_set = pd.read_csv(path + 'test.csv',
+                       index_col=0)
+submission = pd.read_csv(path + 'submission.csv',
+                       index_col=0)
 
-print(x)
-print(x.columns)
-print(x.shape) #(1459,9)
 
-y = train_set['count'] #count 컬럼만 y 변수에 저장한다.
-print(y)
-print(y.shape)
+#### Missing value processing ####
+train_set = train_set.fillna(train_set.mean()) # nan 값을 행별로 모두 삭제(dropna)
+test_set = test_set.fillna(test_set.mean())
 
-x_train,x_test,y_train, y_test = train_test_split(x,y, train_size=0.7, shuffle=True, random_state=72)
 
-scaler = StandardScaler()
+x = train_set.drop(['count'], axis=1) # axis는 'count'가 컬럼이라는 것을 명시하기 위해
+y = train_set['count']
+
+
+x_train, x_test, y_train, y_test = train_test_split(x, y,
+        train_size=0.98, shuffle=True, random_state=68)
+
+scaler = MinMaxScaler()
 scaler.fit(x_train)
-x_train = scaler.transform(x_train) 
+x_train = scaler.transform(x_train)
 x_test = scaler.transform(x_test)
-test_set = scaler.transform(test_set)
-print(np.min(x_train))  # 0.0
-print(np.max(x_train))  # 1.0
 
-print(np.min(x_test))  # 1.0
-print(np.max(x_test))  # 1.0
 
 #2. 모델구성
+allAlgorithms = all_estimators(type_filter='regressor')
+print('모델의 갯수 :', len(allAlgorithms)) # 모델의 갯수 : 41
 
-allalgorithm = all_estimators(type_filter='classifier')
 
-print('allalgorithms : ', allalgorithm)
-print("모델의 갯수 : ", len(allalgorithm)) #모델의 갯수 :  41
 
-for (name, algorithm) in allalgorithm : #name-algorithm : key-value 쌍으로 이루는 dictionary
-  try : 
-      model = algorithm()
-      model.fit(x_train, y_train)
- 
-      y_predict = model.predict(x_test)
-      acc = accuracy_score(y_test,y_predict)
-      print(name, "의 정답률 : ", round(acc,4))
-  except : 
-    #   continue
-    print(name, ": 미출력!!!!!!!!")
+for (name, algorithm) in allAlgorithms:
+    try:
+        model = algorithm()
+        model.fit(x_train, y_train)
+    
+        y_predict = model.predict(x_test)
+        acc = r2_score(y_test, y_predict)
+        print(name, '의 정답률 :', acc)
+    except:
+        # continue
+        print(name, '은 안나온 애들!')
+
+#3. 컴파일, 훈련
+model.fit(x_train, y_train)
+
+#4. 평가, 예측
+result = model.score(x_test, y_test) # evaluate 대신 score 사용
+print('결과 :', result)
+
+from sklearn.metrics import r2_score
+y_predict_1 = model.predict(x_test)
+acc = r2_score(y_test, y_predict_1)
+print('r2 스코어 : ', acc)
+
+# 결과 : 0.11906345565855969
+# r2 스코어 :  0.11906345565855969
