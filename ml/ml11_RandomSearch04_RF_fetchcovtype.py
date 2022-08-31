857903eebfb8ebd42803597aed6cec9f3c67533a
junginee <107666154+junginee@users.noreply.github.com>
2022-08-31 11:50:02 +0900

    Update ml11_RandomSearch04_RF_fetchcovtype.py

--- ml/ml11_RandomSearch04_RF_fetchcovtype.py
+++ ml/ml11_RandomSearch04_RF_fetchcovtype.py
@@ -13,17 +13,7 @@ datasets = fetch_covtype()
 x = datasets.data
 y= datasets.target
 
-print(datasets.feature_names)
-print(datasets.DESCR)
-print(x.shape, y.shape) #(581012, 54) (581012,)
-print(np.unique(y, return_counts = True)) # y :[1 2 3 4 5 6 7]  / return_counts :[211840, 283301,  35754,   2747,   9493,  17367,  20510]
-
-# import pandas as pd
-# y = pd.get_dummies(y)
-# print(y)
-
 x_train, x_test, y_train, y_test = train_test_split( x, y, train_size = 0.8, shuffle=True, random_state=68 )
-
 
 n_splits=5
 kfold = KFold(n_splits=5, shuffle=True, random_state=101)
@@ -41,12 +31,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 
-model =  RandomForestClassifier(max_depth=10, min_samples_split=3)                         
-# model = GridSearchCV(RandomForestClassifier(), parameters, cv=kfold, verbose=1,          
-#                      refit=True, n_jobs=1)                             
-                                                                           
-                                                                          
-                                                                           # 컴퓨터는 뜨거워지겠지만, 속도는 많이 빨라진다.
+model =  RandomForestClassifier(max_depth=10, min_samples_split=3)                       
 
 
 #3. 컴파일, 훈련
