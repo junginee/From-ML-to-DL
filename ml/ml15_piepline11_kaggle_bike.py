d8038b57e012babacbd81ba353f6a58cc2b33d63
junginee <107666154+junginee@users.noreply.github.com>
2022-08-31 12:59:41 +0900

    Update ml15_piepline11_kaggle_bike.py

--- ml/ml15_piepline11_kaggle_bike.py
+++ ml/ml15_piepline11_kaggle_bike.py
@@ -7,7 +7,7 @@ path = './_data/kaggle_bike/'
 train_set = pd.read_csv(path + 'train.csv')
 test_set = pd.read_csv(path + 'test.csv')
 train_set['datetime'] = pd.to_datetime(train_set['datetime']) 
-train_set['year'] = train_set['datetime'].dt.year  # 분과 초는 모든값이 0이므로 추가x
+train_set['year'] = train_set['datetime'].dt.year  
 train_set['month'] = train_set['datetime'].dt.month
 train_set['day'] = train_set['datetime'].dt.day
 train_set['hour'] = train_set['datetime'].dt.hour
@@ -17,7 +17,6 @@ train_set['hour'] = train_set['hour'].astype('category')
 train_set = pd.get_dummies(train_set, columns=['season','weather'])
 train_set.drop(['casual', 'registered'], inplace=True, axis=1)
 train_set.drop('atemp', inplace=True, axis=1)
-
 test_set['datetime'] = pd.to_datetime(test_set['datetime'])
 test_set['month'] = test_set['datetime'].dt.month
 test_set['hour'] = test_set['datetime'].dt.hour
@@ -41,15 +40,13 @@ x_train, x_test, y_train, y_test = train_test_split(x, y,
 #2. 모델구성
 from sklearn.svm import LinearSVC, SVC
 from sklearn. ensemble import RandomForestClassifier, RandomForestRegressor
-
 from sklearn.pipeline import make_pipeline
 
-#model = SVC()
-model = make_pipeline(MinMaxScaler(),RandomForestRegressor())  # piepline을 통해 순서대로 이동
-                                             # 스케일러 -> svc
+
+model = make_pipeline(MinMaxScaler(),RandomForestRegressor()) 
                                              
 #3. 훈련
-model.fit(x_train, y_train)  #piepline의  model.fit에서는 fit과 transform 동시 일어남
+model.fit(x_train, y_train) 
 
 #4. 평가, 예측
 result = model.score(x_test, y_test)
