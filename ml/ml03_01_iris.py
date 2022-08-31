a2ba34030745e2652d5d06b5182fcb342d916b66
junginee <107666154+junginee@users.noreply.github.com>
2022-08-31 10:28:28 +0900

    Update ml03_01_iris.py

--- ml/ml03_01_iris.py
+++ ml/ml03_01_iris.py
@@ -31,15 +31,13 @@ for i in model:
 
 
     #4. 평가, 예측
-
     result = model.score(x_test,y_test)   
-
     y_predict = model.predict(x_test)
-
     print(f"{i} : ", round(result,4))   
 
-    # Perceptron() :  0.7
-'''    
+    
+'''   
+Perceptron() :  0.7
 SVC() :  0.9
 KNeighborsClassifier() :  0.9667
 LogisticRegression() :  0.9667
