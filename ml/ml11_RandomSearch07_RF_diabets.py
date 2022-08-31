1c2af2bbd476ba26629d719915aec57c2e3c58ac
junginee <107666154+junginee@users.noreply.github.com>
2022-08-31 11:54:19 +0900

    Update ml11_RandomSearch07_RF_diabets.py

--- ml/ml11_RandomSearch07_RF_diabets.py
+++ ml/ml11_RandomSearch07_RF_diabets.py
@@ -1,129 +1,86 @@
+parameters =[
+    {'n_estimators': [100,200]}, # epochs 의 수를 조정하는 옵션
+    {'max_depth': [6,8,10,12]}, # 최대 깊이 조정하는 옵션
+    {'min_samples_leaf': [3,5,7,10]}, # 리프 노드의 최소 샘플 수 조정하는 옵션
+    {'min_samples_split': [2,3,5,10]}, # 분할 노드의 최소 샘플 수 조정하는 옵션
+    {'n_jobs': [-1,2,4]} # cpu 수 (병렬 처리 수) 조정하는 옵션 -- 성능이 아닌 속도에만 영향을 줌
+]
+
+# m09_03
+
+from unittest import result
 import numpy as np
-from sklearn.model_selection import train_test_split, KFold, cross_val_score , GridSearchCV
 from sklearn.datasets import load_diabetes
-from sklearn.preprocessing import StandardScaler
-from sklearn.svm import LinearSVR
-from sklearn. ensemble import RandomForestRegressor
-from sklearn.metrics import r2_score, accuracy_score
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
+from sklearn.model_selection import KFold, cross_val_score, GridSearchCV, StratifiedKFold 
+from sklearn.metrics import accuracy_score, r2_score
 
-
-datasets =load_diabetes()
+datasets = load_diabetes()
 x = datasets.data
 y = datasets.target
-x_train,x_test,y_train, y_test = train_test_split(x,y, train_size=0.75, shuffle=True, random_state=72)
 
-scaler = StandardScaler()
-scaler.fit(x_train)
-x_train = scaler.transform(x_train) 
-x_test = scaler.transform(x_test)
-print(np.min(x_train)) #0.0 
-print(np.max(x_train)) #1.0
+print(x)
+print(y)
+print(x.shape, y.shape) #(442, 10) (442,) Number of Instances: 442, Number of Attributes 10
 
-n_splits = 5
-kfold = KFold(n_splits=n_splits, shuffle = True, random_state=120)    
+print(datasets.feature_names)
+print(datasets.DESCR)
+
+
+x_train, x_test, y_train, y_test = train_test_split(x, y,
+        train_size=0.8, shuffle=True, random_state=66)
+
+n_splits=5
+kfold = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=66)
 
 parameters = [
-        {'n_estimators':[100,200], 'max_depth':[6,8,10,12],'min_samples_split':[2,3,5,10]},
-        {'n_estimators':[100,200], 'min_samples_leaf':[3,5,7,10], 'min_samples_split':[2,3,5,10]},
-] 
+    {'n_estimators': [100,200], 'max_depth': [6,8,10,12], 'min_samples_leaf': [3,5,7,10]},          # 32번
+    {'max_depth': [6,8,10,12], 'min_samples_leaf': [3,5,7,10], 'min_samples_split': [2,3,5,10]},    # 64번
+    {'min_samples_leaf': [3,5,7,10], 'min_samples_split': [2,3,5,10], 'n_jobs': [-1,2,4]}           # 48번 = 총 144번
+]
+
 
 #2. 모델구성
-from sklearn.svm import LinearSVC, SVC
-from sklearn.linear_model import Perceptron, LogisticRegression # LogisticRegression는 분류임
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
-
-model =  RandomForestRegressor(min_samples_leaf=3, min_samples_split=5)                        
-# model = GridSearchCV(RandomForestRegressor(), parameters, cv=kfold, verbose=1,          
-#                      refit=True, n_jobs=1)                             
-                                                                           
-                                                                          
-                                                                          
+from sklearn.ensemble import RandomForestClassifier,RandomForestRegressor 
+model = RandomizedSearchCV(RandomForestRegressor(), parameters, cv=kfold, verbose=1,
+                     refit=True, n_jobs=-1)
 
 
 #3. 컴파일, 훈련
+import time
+start = time.time()
 model.fit(x_train, y_train)
+end = time.time()
 
-####### GridSearchCV 탐색결과 #######
-# print("최적의 매개변수 : ", model.best_estimator_)
-# 최적의 매개변수 :  RandomForestRegressor(min_samples_leaf=10, min_samples_split=3)
-# print("최적의 파라미터 : ", model.best_params_)
-# 최적의 파라미터 :  {'min_samples_leaf': 10, 'min_samples_split': 3, 'n_estimators': 100}
-# print("best_score_ : ", model.best_score_)
-# best_score_ :  0.4353879532270981
-# print("model.score : ", model.score(x_test, y_test))
-# model.score :  0.5845886025214724
-###################################### 
+print("최적의 매개변수 : ", model.best_estimator_) 
+print("최적의 파라미터  : ", model.best_params_)
+print('best_score : ', model.best_score_)
+print('model.score : ', model.score(x_test, y_test))
 
-#4. 평가
 y_predict = model.predict(x_test)
-print("r2_score", round(r2_score(y_test, y_predict),4))
-# r2_score 0.5834
+print('r2_score : ', r2_score(y_test, y_predict))
 
-# y_pred_best = model.best_estimator_.__prepare__(x_test)
-# print('최적 튠 ACC : ', accuracy_score(y_test, y_pred_best))import numpy as np
-from sklearn.model_selection import train_test_split, KFold, cross_val_score , GridSearchCV
-from sklearn.datasets import load_diabetes
-from sklearn.preprocessing import StandardScaler
-from sklearn.svm import LinearSVR
-from sklearn. ensemble import RandomForestRegressor
-from sklearn.metrics import r2_score, accuracy_score
+y_pred_best = model.best_estimator_.predict(x_test)
+print('최적 튠 ACC :', r2_score(y_test, y_pred_best))
+print('걸린시간 :', np.round(end-start, 4), "초")
 
 
-datasets =load_diabetes()
-x = datasets.data
-y = datasets.target
-x_train,x_test,y_train, y_test = train_test_split(x,y, train_size=0.75, shuffle=True, random_state=72)
-
-scaler = StandardScaler()
-scaler.fit(x_train)
-x_train = scaler.transform(x_train) 
-x_test = scaler.transform(x_test)
-print(np.min(x_train)) #0.0 
-print(np.max(x_train)) #1.0
-
-n_splits = 5
-kfold = KFold(n_splits=n_splits, shuffle = True, random_state=120)    
-
-parameters = [
-        {'n_estimators':[100,200], 'max_depth':[6,8,10,12],'min_samples_split':[2,3,5,10]},
-        {'n_estimators':[100,200], 'min_samples_leaf':[3,5,7,10], 'min_samples_split':[2,3,5,10]},
-] 
-
-#2. 모델구성
-from sklearn.svm import LinearSVC, SVC
-from sklearn.linear_model import Perceptron, LogisticRegression # LogisticRegression는 분류임
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
-
-model =  RandomForestRegressor(min_samples_leaf=3, min_samples_split=5)                        
-# model = GridSearchCV(RandomForestRegressor(), parameters, cv=kfold, verbose=1,          
-#                      refit=True, n_jobs=1)                             
-                                                                           
-                                                                          
-                                                                          
+# RandomizedSearchCV
+# 최적의 매개변수 :  RandomForestRegressor(max_depth=12, min_samples_leaf=10, min_samples_split=10)     
+# 최적의 파라미터  :  {'min_samples_split': 10, 'min_samples_leaf': 10, 'max_depth': 12}
+# best_score :  0.47497523752934995
+# model.score :  0.4071301230026233
+# r2_score :  0.4071301230026233
+# 최적 튠 ACC : 0.4071301230026233
+# 걸린시간 : 5.8177 초
 
 
-#3. 컴파일, 훈련
-model.fit(x_train, y_train)
-
-####### GridSearchCV 탐색결과 #######
-# print("최적의 매개변수 : ", model.best_estimator_)
-# 최적의 매개변수 :  RandomForestRegressor(min_samples_leaf=10, min_samples_split=3)
-# print("최적의 파라미터 : ", model.best_params_)
-# 최적의 파라미터 :  {'min_samples_leaf': 10, 'min_samples_split': 3, 'n_estimators': 100}
-# print("best_score_ : ", model.best_score_)
-# best_score_ :  0.4353879532270981
-# print("model.score : ", model.score(x_test, y_test))
-# model.score :  0.5845886025214724
-###################################### 
-
-#4. 평가
-y_predict = model.predict(x_test)
-print("r2_score", round(r2_score(y_test, y_predict),4))
-# r2_score 0.5834
-
-# y_pred_best = model.best_estimator_.__prepare__(x_test)
-# print('최적 튠 ACC : ', accuracy_score(y_test, y_pred_best))
+# GridSearchCV
+# Fitting 5 folds for each of 144 candidates, totalling 720 fits
+# 최적의 매개변수 :  RandomForestRegressor(max_depth=6, min_samples_leaf=10)
+# 최적의 파라미터  :  {'max_depth': 6, 'min_samples_leaf': 10, 'n_estimators': 100}
+# best_score :  0.4829903742012565
+# model.score :  0.40448589011887204
+# r2_score :  0.40448589011887204
+# 최적 튠 ACC : 0.40448589011887204
+# 걸린시간 : 18.1175 초
