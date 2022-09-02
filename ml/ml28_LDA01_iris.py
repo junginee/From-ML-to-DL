04bd941ea7cc8e23340b222b7803ae0f4a9db595
junginee <107666154+junginee@users.noreply.github.com>
2022-09-03 00:03:42 +0900

    Update ml28_LDA01_iris.py

--- ml/ml28_LDA01_iris.py
+++ ml/ml28_LDA01_iris.py
@@ -1,8 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn.datasets import load_iris, load_breast_cancer
-from sklearn.datasets import load_wine, fetch_covtype
-from sklearn.datasets import load_digits
+from sklearn.datasets import load_iris
 from xgboost import XGBClassifier
 from sklearn.decomposition import PCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
@@ -16,22 +14,12 @@ y = datasets.target
 print(x.shape) #(150, 4)
 
 from sklearn.preprocessing import MinMaxScaler, StandardScaler, MaxAbsScaler, RobustScaler
-# scaler = MinMaxScaler()
 scaler = StandardScaler()
-# scaler = MaxAbsScaler()
-# scaler = RobustScaler()
+
 
 x = scaler.fit_transform(x)
 le = LabelEncoder()
 y = le.fit_transform(y)
-
-# pca = PCA(n_components=20)  # 54 -> 10 
-# x = pca.fit_transform(x)
-# print(x.shape)  
-
-# pca_EVR = pca.explained_variance_ratio_
-# cumsum = np.cumsum(pca_EVR) 
-# print(cumsum)
 
 # LDA 
 lda = LinearDiscriminantAnalysis(n_components=2)
@@ -41,15 +29,6 @@ print(x.shape) #(150, 2)
 
 x_train, x_test, y_train, y_test = train_test_split(
     x, y, train_size=0.8, random_state=72, shuffle=True, stratify=y)
-
-# LDA 
-# lda = LinearDiscriminantAnalysis(n_components=6)
-# lda.fit(x_train, y_train)
-# x_train = lda.transform(x_train)
-# x_test = lda.transform(x_test)
-print(x_train.shape, x_test.shape)  # (120, 2) (30, 2)
-print(np.unique(y_train, return_counts=True))   
-#(array([0, 1, 2], dtype=int64), array([40, 40, 40], dtype=int64))
 
 #2. 모델
 model = XGBClassifier(tree_method='gpu_hist', predictor='gpu_predictor', gpu_id=0)
@@ -75,7 +54,6 @@ print('걸린 시간 : ', end - start)
 # XGBClassifier - gpu
 # 결과 :  0.872421538170271      
 # 걸린 시간 :  5.9461281299591064
-
 
 # XGBClassifier - gpu / n_component : 10
 # 결과 :  0.8419403974079843      
