973a2bc9fb2513ab20a1fda918c28401e525b108
junginee <107666154+junginee@users.noreply.github.com>
2022-09-02 11:04:39 +0900

    Update ml27_pca_mnist3_xgb_gridSearch1.py

--- ml/ml27_pca_mnist3_xgb_gridSearch1.py
+++ ml/ml27_pca_mnist3_xgb_gridSearch1.py
@@ -1,99 +1,49 @@
-
-# n_component > 0.95 이상 (pca를 통해 0.95이상인 n_components 가 몇개인지?)
-# xgboost, gridSearch 또는 randomSearch를 쓸것
-# m27_2 결과를 뛰어넘어라
-
-import numpy as np     
-from keras.datasets import mnist
+from tabnanny import verbose
+import numpy as np
 from sklearn.decomposition import PCA
-from sklearn.metrics import accuracy_score
-from xgboost.sklearn import XGBClassifier
+from keras.datasets import mnist
+from xgboost import XGBClassifier
+from sklearn.model_selection import RandomizedSearchCV
+import time
+import warnings
+warnings.filterwarnings(action='ignore')
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data() 
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# print(x_train.shape, x_test.shape) #(60000, 28, 28) (10000, 28, 28)
 
-# 1. 데이터
+x_train = x_train.reshape(x_train.shape[0], x_train.shape[1]*x_train.shape[2])
+x_test = x_test.reshape(x_test.shape[0], x_test.shape[1]*x_test.shape[2])
 x = np.append(x_train, x_test, axis=0)
-y = np.append(y_train, y_test, axis=0)
-
-x = x.reshape(70000, 784)
-
-pca = PCA(n_components=784-331) 
-x = pca.fit_transform(x)
-
-# 주성분 분석이 어느정도 영향을 미치는지 알고 있다면 판단 하는게 쉬워짐 
-# 차원축소의 비율에 대해서 확인함 
-pca_EVR = pca.explained_variance_ratio_
-# print(pca_EVR)
-
-cumsum = np.cumsum(pca_EVR)
-# print(cumsum) 
-
-# print(np.argmax(cumsum >= 0.99)+1) #331
-# import matplotlib.pyplot as plt     
-# plt.plot(cumsum)
-# plt.grid() 
-# plt.show()
-
-# print(x_train.shape, y_train.shape)
-# print(x.shape) #(70000, 630)
-
-x_train = x[:60000]
-x_test = x[60000:70000]
-y_train = y[:60000]
-y_test = y[60000:70000]
-
-
-from xgboost import XGBRegressor, XGBClassifier
-from xgboost.plotting import plot_importance
-
-model = XGBClassifier()
-
-from sklearn.model_selection import KFold, GridSearchCV, RandomizedSearchCV
-
-# GridSearchCV 
-# 모델, 러닝레이트, 크로스발리데이션까지 한꺼번에 랩으로 싸겠다. 
-# 그리고 이 자체를 하나의 모델로 본다 
-# 이 모델은 기존 모델과 우리가 넣고싶은 하이퍼 파라미터의모임을 딕셔너리 형태로 구성해주고
-# 크로스 발리데이션까지 명시해주면 모델의 형태가 된다.
-# 크로스 발리데이션만큼 돌아간다 cv를 5번 주면 모델이 5번 돌아가고 
-# 노드가 3개면 15번 돌아가고 러닝레이트가 3이면 45번 돌아간다 
-
-
-kfold = KFold(n_splits=5, shuffle=True, random_state=66)
-#n_splits=5 전체 데이터를 5등분해서 5번 반복해준다 = 20% test  (등분한 수만큼 반복을 해준다) 
-#cross_val_score = 교차검증 방법
 
 parameters = [
-    {'n_estimators' : [100,200,300], "learning_rate":[0.1,0.3,0.001,0.01],
-     'max_depth' : [4, 5, 6]}
-    ,{'n_estimators' : [90,100,110], "learning_rate":[0.1,0.001,0.01],
-    'max_depth' : [4, 5, 6], 'colsample_bytree':[0.6,0.9,1]},
-    {'n_estimators' : [90,110], "learning_rate":[0.1,0.3,0.001,0.5],
-     'max_depth' : [4,5,6],
-    'colsample_bytree':[0.6,0.9,1]}
+{'n_estimators':[100,200,300], 'learning_rate':[0.1, 0.3, 0.001, 0.01], 'max_depth':[4,5,6]},
+{'n_estimators':[90,100,110], 'learning_rate':[0.1, 0.001, 0.01], 'max_depth':[4,5,6], 'colsample_bytree':[0.6,0.9,1]},
+{'n_estimators':[90,110], 'learning_rate':[0.1, 0.001, 0.5], 'max_depth':[4,5,6],'colsample_bytree':[0.6,0.7,0.9]}
 ]
-n_jobs = -1
 
-# 2. model 구성 
-model = RandomizedSearchCV(XGBClassifier(), parameters, cv=kfold)
-#파라미터와 cv를 곱한것만큼 돌아간다 SVC에는 여러가지 파라미터가 존재한다 
-#gridSearch에서는 fit을 지원한다 
-# model = SVC()
+model = RandomizedSearchCV(XGBClassifier(tree_method='gpu_hist', predictor='gpu_predictor', 
+                                         gpu_id=1), parameters, verbose=1, refit=True, n_jobs=-1)
 
-# 3. 훈련
-model.fit(x_train,y_train)
+pca = PCA(n_components=x_train.shape[1])
+x = pca.fit_transform(x)
+pca_EVR = pca.explained_variance_ratio_
+cumsum = np.cumsum(pca_EVR)
 
+pca = PCA(n_components=np.argmax(cumsum >= 0.999)+1)
+x2_train = pca.fit_transform(x_train)
+x2_test = pca.transform(x_test)
+start = time.time()
+model.fit(x2_train, y_train, verbose=1)
+end = time.time()
 
-# 4. 평가, 예측 
-print("최적의 매개변수 : ", model.best_estimator_) #best_estimator_ 가장 좋은 평가가 무엇인가? 
+results = model.score(x2_test, y_test)
+print('결과: ', results)
+print('시간: ', end-start)
 
-print("best_score_ : ", model.best_score_)
+# PCA 0.95 이상
+# 결과:  0.9649
+# 시간:  510.00438714027405
 
-print("model.score :", model.score(x_test, y_test))
-
-y_pred = model.predict(x_test)
-print("accuracy_score : ", accuracy_score(y_test, y_pred))
-
-
+# PCA 0.99 이상
+# 결과:  0.9307
+# 시간:  527.3115980625153
