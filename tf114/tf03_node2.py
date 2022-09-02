a6c9e95f29360c7ccc5a15f0d3cd5647454aef1d
junginee <107666154+junginee@users.noreply.github.com>
2022-09-02 11:11:08 +0900

    Update tf03_node2.py

--- tf114/tf03_node2.py
+++ tf114/tf03_node2.py
@@ -1,9 +1,3 @@
-#[실습]
-# 덧셈 node3
-# 뺄셈 node4
-# 곱셈 node5
-# 나누셈 node6
-
 import tensorflow as tf
 
 node1 = tf.constant(2.0, tf.float32)
@@ -15,16 +9,15 @@ node6 = tf.div(node1, node2)
 
 
 sess = tf.Session()
-print(sess.run(node3)) #덧셈
+print(sess.run(node3)) 
 print()
-print(sess.run(node4)) #뺄셈
+print(sess.run(node4)) 
 print()
-print(sess.run(node5)) #곱셈
+print(sess.run(node5)) 
 print()
-print(sess.run(node6)) #나누셈
+print(sess.run(node6))
 
 import tensorflow as tf
 node1 = tf.constant(1,2,3)
 node2 = tf.constant(4,5,6)
 tf.print(node1, node2)
-
