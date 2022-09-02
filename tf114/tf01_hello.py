0763496cb854e0e3e3ee1b32265b2f480d65ac46
junginee <107666154+junginee@users.noreply.github.com>
2022-09-02 11:06:55 +0900

    Update tf01_hello.py

--- tf114/tf01_hello.py
+++ tf114/tf01_hello.py
@@ -1,19 +1,4 @@
-# import tensorflow as tf
-# print(tf.__version__)
-
-# # print("hello world")        
-
-# hello = tf.constant("hello world")           
-# # print(hello)
-
-# sess = tf.compat.v1.Session()
-# print(sess.run(hello))
-   
-import tensorflow.compat.v1 as tf
-
-# import numpy as np
-
- 
+import tensorflow.compat.v1 as tf 
 
 hello = tf.constant("hello world")
 
