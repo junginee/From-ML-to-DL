00b5e5c93c9c058a764f38d2063c06127f87a77b
junginee <107666154+junginee@users.noreply.github.com>
2022-09-04 15:50:55 +0900

    Update ml33_outlier12_kaggle_house.py

--- ml/ml33_outlier12_kaggle_house.py
+++ ml/ml33_outlier12_kaggle_house.py
@@ -14,7 +14,7 @@ from sklearn.impute import SimpleImputer
 
 #1.data 처리
 
-############# id컬럼 index 처리 ##########
+# id column index processing
 path = './_data/kaggle_house/'
 train_set = pd.read_csv(path + 'train.csv')
 test_set = pd.read_csv(path + 'test.csv')
@@ -25,9 +25,9 @@ test_set.set_index('Id', inplace=True)
 test_id_index = train_set.index
 trainLabel = train_set['SalePrice']
 train_set.drop(['SalePrice'], axis=1, inplace=True)
-############################################
+###########################################
 
-###########이상치 처리######################
+# Outliers Processed 
 def dr_outlier(train_set):
     quartile_1 = train_set.quantile(0.25)
     quartile_3 = train_set.quantile(0.75)
@@ -40,59 +40,49 @@ def dr_outlier(train_set):
 
 dr_outlier(train_set)
 
-############ 트레인,테스트 합치기 ############
+# train, test concat 
 alldata = pd.concat((train_set, test_set), axis=0)
 alldata_index = alldata.index
 
 
-############ NA 값 20프로 이상은 drop! ##########
+# If the NA value is more than 20%, drop 
 NA_Ratio = 0.8 * len(alldata)
 alldata.dropna(axis=1, thresh=NA_Ratio, inplace=True)
 
-################### 수치형,카테고리형 분리,범위 설정 #############
+# Numerical, Category Separation, Range Setting
 alldata_obj = alldata.select_dtypes(include='object') 
 alldata_num = alldata.select_dtypes(exclude='object')
 
 for objList in alldata_obj:
     alldata_obj[objList] = LabelEncoder().fit_transform(alldata_obj[objList].astype(str))
 
-
-##################### 소수 na 값 처리 ###################    
+# Processing decimal na values   
 imputer = SimpleImputer(strategy='mean')
 imputer.fit(alldata_num)
 alldata_impute = imputer.transform(alldata_num)
 alldata_num = pd.DataFrame(alldata_impute, columns=alldata_num.columns, index=alldata_index)  
 
-###################### 분리한 데이터 다시 합치기 #####################
+# Re-combining separated data 
 alldata = pd.merge(alldata_obj, alldata_num, left_index=True, right_index=True)  
 
-##################### 트레인, 테스트 다시 나누기 ##################
 train_set = alldata[:len(train_set)]
 test_set = alldata[len(train_set):]
 
-############### 트레인 데이터에 sale price 합치기 ##############
 train_set['SalePrice'] = trainLabel
 
-############### sale price 다시 드랍 #####################
 train_set = train_set.drop(['SalePrice'], axis =1)
 print(train_set)
 print(trainLabel)
 print(test_set)
 
-###############################################################
 x_train, x_test, y_train, y_test = train_test_split(train_set, trainLabel, train_size=0.8, 
                                                      random_state=58)
 
-# minmax , standard 트레인테스트를 나눈뒤 해야한다. 과적합 방지와 같은 이치 
-# scaler = MinMaxScaler()
-# scaler = StandardScaler()
-# scaler = MaxAbsScaler()
 scaler = RobustScaler()
-
 scaler.fit(x_train)
-x_train = scaler.transform(x_train) #스케일링한것을 보여준다.
-x_test = scaler.transform(x_test) #test는 transfrom만 해야됨 
-test_set = scaler.transform(test_set) # **최종테스트셋이 있는경우 여기도 스케일링을 적용해야함 **               
+x_train = scaler.transform(x_train) 
+x_test = scaler.transform(x_test) 
+test_set = scaler.transform(test_set)      
                                                     
 #2.모델구성
 model = Sequential()
@@ -116,7 +106,7 @@ print('loss: ', loss)
 y_predict = model.predict(x_test).flatten()
 
 def RMSE(y_test, y_predict):
-    return np.sqrt(mean_squared_error(y_test, y_predict)) #sqrt: 루트 씌우기
+    return np.sqrt(mean_squared_error(y_test, y_predict))
 
 y_submmit = model.predict(test_set)
 
