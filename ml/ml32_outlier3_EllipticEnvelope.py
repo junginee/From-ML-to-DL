7c65314f44795e08efb1fb7ca92b6f29d7d7f32e
junginee <107666154+junginee@users.noreply.github.com>
2022-09-04 15:39:45 +0900

    Update ml32_outlier3_EllipticEnvelope.py

--- ml/ml32_outlier3_EllipticEnvelope.py
+++ ml/ml32_outlier3_EllipticEnvelope.py
@@ -3,7 +3,7 @@ aaa = np.array([-10,2,3,4,5,6,7,8,9,10,11,12,50])
 aaa = aaa.reshape(1,-1)
 from sklearn.covariance import EllipticEnvelope
 outliers = EllipticEnvelope(contamination=.3) 
-                                          #.숫자 = 이 범위의 데이터를 이상치로 설정
+                                          #.숫자 = 이 범위의 데이터를 이상치로 설정   
 
 outliers.fit(aaa)
 results = outliers.predict(aaa)
