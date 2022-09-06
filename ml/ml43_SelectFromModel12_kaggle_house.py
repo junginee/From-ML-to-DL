850c678b89698c435c2ff5c6ae7c16860f54b0f8
junginee <107666154+junginee@users.noreply.github.com>
2022-09-06 18:56:17 +0900

    Update ml43_SelectFromModel12_kaggle_house.py

--- ml/ml43_SelectFromModel12_kaggle_house.py
+++ ml/ml43_SelectFromModel12_kaggle_house.py
@@ -1,259 +1,141 @@
 import numpy as np
+from sklearn.datasets import fetch_covtype
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import KFold, StratifiedKFold
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from xgboost import XGBClassifier, XGBRegressor
+import time
+from sklearn.preprocessing import LabelEncoder
+from tqdm import tqdm_notebook
 import pandas as pd
-from collections import Counter
-from sklearn.experimental import enable_halving_search_cv
-from sklearn.model_selection import train_test_split, KFold,\
-    HalvingRandomSearchCV
-from sklearn.metrics import r2_score
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import accuracy_score, r2_score
 from sklearn.feature_selection import SelectFromModel
-from xgboost import XGBRegressor
-import warnings
-warnings.filterwarnings('ignore') # warnig 출력 안함
-
-encording_columns = ['MSZoning','Street','Alley','LotShape','LandContour','Utilities','LotConfig',
-                    'LandSlope','Neighborhood','Condition1','Condition2','BldgType','HouseStyle',
-                    'RoofStyle','RoofMatl','Exterior1st','Exterior2nd','MasVnrType','ExterQual',
-                    'ExterCond','Foundation','BsmtQual','BsmtCond','BsmtExposure','BsmtFinType1',
-                    'BsmtFinType2','Heating','HeatingQC','CentralAir','Electrical','KitchenQual',
-                    'Functional','FireplaceQu','GarageType','GarageFinish','GarageQual','GarageCond',
-                    'PavedDrive','PoolQC','Fence','MiscFeature','SaleType','SaleCondition']
-
-non_encording_columns = ['MSSubClass','LotFrontage','LotArea','OverallQual','OverallCond',
-                         'YearBuilt','YearRemodAdd','MasVnrArea','BsmtFinSF1','BsmtFinSF2',
-                         'BsmtUnfSF','TotalBsmtSF','1stFlrSF','2ndFlrSF','LowQualFinSF',
-                         'GrLivArea','BsmtFullBath','BsmtHalfBath','FullBath','HalfBath','BedroomAbvGr',
-                         'KitchenAbvGr','TotRmsAbvGrd','Fireplaces','GarageYrBlt','GarageCars','GarageArea',
-                         'WoodDeckSF','OpenPorchSF','EnclosedPorch','3SsnPorch','ScreenPorch','PoolArea',
-                         'MiscVal','MoSold','YrSold']
-
 
 
 #1. 데이터
-path = 'D:\study_data\_data\kaggle_house/'
-train_set = pd.read_csv(path + 'train.csv') 
-            
-test_set = pd.read_csv(path + 'test.csv') 
+path = './_data/kaggle_house/' 
+train_set = pd.read_csv(path + 'train.csv',
+                        index_col=0) 
+#print(train_set)
+#print(train_set.shape) # (1460, 80) 
 
-###################### IQR 이용해서 train_set에서 이상치나온 행 삭제########################
-def detect_outliers(df, n, features):
-    outlier_indices = []
-    for col in features:
-        Q1 = np.percentile(df[col], 25)
-        Q3 = np.percentile(df[col], 75)
-        IQR = Q3 - Q1
-        
-        outlier_step = 1.5 * IQR
-        
-        outlier_list_col = df[(df[col] < Q1 - outlier_step) | (df[col] > Q3 + outlier_step)].index
-        outlier_indices.extend(outlier_list_col)
-    outlier_indices = Counter(outlier_indices)
-    multiple_outliers = list(k for k, v in outlier_indices.items() if v > n)
-        
-    return multiple_outliers
-        
-Outliers_to_drop = detect_outliers(train_set, 2, ['MSSubClass', 'LotFrontage', 'LotArea', 'OverallQual',
-       'OverallCond', 'YearBuilt', 'YearRemodAdd', 'MasVnrArea', 'BsmtFinSF1',
-       'BsmtFinSF2', 'BsmtUnfSF', 'TotalBsmtSF', '1stFlrSF', '2ndFlrSF',
-       'LowQualFinSF', 'GrLivArea', 'BsmtFullBath', 'BsmtHalfBath', 'FullBath',
-       'HalfBath', 'BedroomAbvGr', 'KitchenAbvGr', 'TotRmsAbvGrd',
-       'Fireplaces', 'GarageYrBlt', 'GarageCars', 'GarageArea', 'WoodDeckSF',
-       'OpenPorchSF', 'EnclosedPorch', '3SsnPorch', 'ScreenPorch', 'PoolArea',
-       'MiscVal', 'MoSold', 'YrSold'])
+test_set = pd.read_csv(path + 'test.csv',
+                       index_col=0)
 
+drop_cols = ['Alley', 'PoolQC', 'Fence', 'MiscFeature'] 
+test_set.drop(drop_cols, axis = 1, inplace =True)
 
-train_set.loc[Outliers_to_drop]
+sample_submission = pd.read_csv(path + 'sample_submission.csv',
+                       index_col=0)
 
+train_set.drop(drop_cols, axis = 1, inplace =True)
+cols = ['MSZoning', 'Street','LandContour','Neighborhood','Condition1','Condition2',
+                'RoofStyle','RoofMatl','Exterior1st','Exterior2nd','MasVnrType','Foundation',
+                'Heating','GarageType','SaleType','SaleCondition','ExterQual','ExterCond','BsmtQual','BsmtCond','BsmtExposure','BsmtFinType1',
+                'BsmtFinType2','HeatingQC','CentralAir','Electrical','KitchenQual','Functional',
+                'FireplaceQu','GarageFinish','GarageQual','GarageCond','PavedDrive','LotShape',
+                'Utilities','LandSlope','BldgType','HouseStyle','LotConfig']
 
-train_set = train_set.drop(Outliers_to_drop, axis = 0).reset_index(drop=True)
-train_set.shape
+for col in tqdm_notebook(cols):
+    le = LabelEncoder()
+    train_set[col]=le.fit_transform(train_set[col])
+    test_set[col]=le.fit_transform(test_set[col])
 
-print(train_set)
+#### 결측치 처리 ####
+# print(train_set.isnull().sum()) 
+train_set = train_set.fillna(train_set.mean()) 
+# print(train_set.isnull().sum())
+# print(train_set.shape) 
+test_set = test_set.fillna(test_set.mean())
 
-num_strong_corr = ['SalePrice','OverallQual','TotalBsmtSF','GrLivArea','GarageCars',
-                   'FullBath','YearBuilt','YearRemodAdd']
+x = train_set.drop(['SalePrice'], axis=1) 
+# print(x)
+# print(x.columns)
+# print(x.shape) # (1460, 79)
 
-num_weak_corr = ['MSSubClass', 'LotFrontage', 'LotArea', 'OverallCond', 'MasVnrArea', 'BsmtFinSF1',
-                 'BsmtFinSF2', 'BsmtUnfSF', '1stFlrSF', '2ndFlrSF','LowQualFinSF', 'BsmtFullBath',
-                 'BsmtHalfBath', 'HalfBath', 'BedroomAbvGr', 'KitchenAbvGr', 'TotRmsAbvGrd',
-                 'Fireplaces', 'GarageYrBlt', 'GarageArea', 'WoodDeckSF','OpenPorchSF',
-                 'EnclosedPorch', '3SsnPorch', 'ScreenPorch', 'PoolArea', 'MiscVal', 'MoSold', 'YrSold']
-
-catg_strong_corr = ['MSZoning', 'Neighborhood', 'Condition2', 'MasVnrType', 'ExterQual',
-                    'BsmtQual','CentralAir', 'Electrical', 'KitchenQual', 'SaleType']
-
-catg_weak_corr = ['Street', 'Alley', 'LotShape', 'LandContour', 'Utilities', 'LotConfig', 
-                  'LandSlope', 'Condition1',  'BldgType', 'HouseStyle', 'RoofStyle', 
-                  'RoofMatl', 'Exterior1st', 'Exterior2nd', 'ExterCond', 'Foundation', 
-                  'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2', 'Heating', 
-                  'HeatingQC', 'Functional', 'FireplaceQu', 'GarageType', 'GarageFinish', 
-                  'GarageQual', 'GarageCond', 'PavedDrive', 'PoolQC', 'Fence', 'MiscFeature', 
-                  'SaleCondition' ]
-
-cols_fillna = ['PoolQC','MiscFeature','Alley','Fence','MasVnrType','FireplaceQu',
-               'GarageQual','GarageCond','GarageFinish','GarageType', 'Electrical',
-               'KitchenQual', 'SaleType', 'Functional', 'Exterior2nd', 'Exterior1st',
-               'BsmtExposure','BsmtCond','BsmtQual','BsmtFinType1','BsmtFinType2',
-               'MSZoning', 'Utilities']
-
-for col in cols_fillna : 
-    train_set[col].fillna('None', inplace=True)
-    test_set[col].fillna('None', inplace=True)
-
-total = train_set.isnull().sum().sort_values(ascending=False)
-percent = (train_set.isnull().sum()/train_set.isnull().count()).sort_values(ascending=False)
-missing_data = pd.concat([total, percent], axis=1, keys=['Total','Percent'])
-
-train_set.fillna(train_set.mean(), inplace=True)
-test_set.fillna(test_set.mean(), inplace=True)
-
-total = train_set.isnull().sum().sort_values(ascending=False)
-percent = (train_set.isnull().sum()/train_set.isnull().count()).sort_values(ascending=False)
-missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
-
-print(train_set.isnull().sum().sum(), test_set.isnull().sum().sum())
-
-id_test = test_set['Id']
-
-to_drop_num = num_weak_corr
-to_drop_catg = catg_weak_corr
-
-cols_to_drop = ['Id'] + to_drop_num + to_drop_catg
-
-for df in [train_set, test_set] :
-    df.drop(cols_to_drop, inplace=True, axis = 1)
-    
-# 'MSZoning'
-msz_catg2 = ['RM', 'RH']
-msz_catg3 = ['RL', 'FV'] 
-
-# Neighborhood
-nbhd_catg2 = ['Blmngtn', 'ClearCr', 'CollgCr', 'Crawfor', 'Gilbert', 'NWAmes', 'Somerst', 'Timber', 'Veenker']
-nbhd_catg3 = ['NoRidge', 'NridgHt', 'StoneBr']
-
-# Condition2
-cond2_catg2 = ['Norm', 'RRAe']
-cond2_catg3 = ['PosA', 'PosN'] 
-
-# SaleType
-SlTy_catg1 = ['Oth']
-SlTy_catg3 = ['CWD']
-SlTy_catg4 = ['New', 'Con']
-
-for df in [train_set, test_set]:
-    
-    df['MSZ_num'] = 1  
-    df.loc[(df['MSZoning'].isin(msz_catg2) ), 'MSZ_num'] = 2    
-    df.loc[(df['MSZoning'].isin(msz_catg3) ), 'MSZ_num'] = 3        
-    
-    df['NbHd_num'] = 1       
-    df.loc[(df['Neighborhood'].isin(nbhd_catg2) ), 'NbHd_num'] = 2    
-    df.loc[(df['Neighborhood'].isin(nbhd_catg3) ), 'NbHd_num'] = 3    
-
-    df['Cond2_num'] = 1       
-    df.loc[(df['Condition2'].isin(cond2_catg2) ), 'Cond2_num'] = 2    
-    df.loc[(df['Condition2'].isin(cond2_catg3) ), 'Cond2_num'] = 3    
-    
-    df['Mas_num'] = 1       
-    df.loc[(df['MasVnrType'] == 'Stone' ), 'Mas_num'] = 2 
-    
-    df['ExtQ_num'] = 1       
-    df.loc[(df['ExterQual'] == 'TA' ), 'ExtQ_num'] = 2     
-    df.loc[(df['ExterQual'] == 'Gd' ), 'ExtQ_num'] = 3     
-    df.loc[(df['ExterQual'] == 'Ex' ), 'ExtQ_num'] = 4     
-   
-    df['BsQ_num'] = 1          
-    df.loc[(df['BsmtQual'] == 'Gd' ), 'BsQ_num'] = 2     
-    df.loc[(df['BsmtQual'] == 'Ex' ), 'BsQ_num'] = 3     
- 
-    df['CA_num'] = 0          
-    df.loc[(df['CentralAir'] == 'Y' ), 'CA_num'] = 1    
-
-    df['Elc_num'] = 1       
-    df.loc[(df['Electrical'] == 'SBrkr' ), 'Elc_num'] = 2 
-
-
-    df['KiQ_num'] = 1       
-    df.loc[(df['KitchenQual'] == 'TA' ), 'KiQ_num'] = 2     
-    df.loc[(df['KitchenQual'] == 'Gd' ), 'KiQ_num'] = 3     
-    df.loc[(df['KitchenQual'] == 'Ex' ), 'KiQ_num'] = 4      
-    
-    df['SlTy_num'] = 2       
-    df.loc[(df['SaleType'].isin(SlTy_catg1) ), 'SlTy_num'] = 1  
-    df.loc[(df['SaleType'].isin(SlTy_catg3) ), 'SlTy_num'] = 3  
-    df.loc[(df['SaleType'].isin(SlTy_catg4) ), 'SlTy_num'] = 4 
-
-train_set.drop(['MSZoning','Neighborhood' , 'Condition2', 'MasVnrType', 'ExterQual', 'BsmtQual','CentralAir', 'Electrical', 'KitchenQual', 'SaleType', 'Cond2_num', 'Mas_num', 'CA_num', 'Elc_num', 'SlTy_num'], axis = 1, inplace = True)
-test_set.drop(['MSZoning', 'Neighborhood' , 'Condition2', 'MasVnrType', 'ExterQual', 'BsmtQual','CentralAir', 'Electrical', 'KitchenQual', 'SaleType', 'Cond2_num', 'Mas_num', 'CA_num', 'Elc_num', 'SlTy_num'], axis = 1, inplace = True)
-
-x = train_set.drop(['SalePrice'], axis=1)
 y = train_set['SalePrice']
+# print(y)
+# print(y.shape) # (1460, )
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.75, random_state=99)
+x_train, x_test, y_train, y_test = train_test_split(x, y,
+    shuffle=True, random_state=123, train_size=0.8, #stratify=y
+)
 
 scaler = MinMaxScaler()
 x_train = scaler.fit_transform(x_train)
 x_test = scaler.transform(x_test)
 
-# 2. 모델
-model = XGBRegressor(n_estimators=100,
-              learning_rate=1,
-              max_depth=2,
-              gamma=0,
-              min_child_weight=1,
-              subsample=1,
-              colsample_bytree=0.5,
-              colsample_bylevel=1,
-              colsample_bynode=1,
-              reg_alpha=0.01,
-              reg_lambd=1,
-              tree_method='gpu_hist', predictor='gpu_predictor', gpu_id=0, random_state=1234,
-              )
+# n_splits = 5
+# kfold = KFold(n_splits=n_splits, shuffle=True, random_state=123)
+
+
+#2. 모델
+model = XGBRegressor(tree_method='gpu_hist', predictor='gpu_predictor', gpu_id=0,
+                      random_state=123, 
+                      n_estimators=100,
+                      learning_rate = 0.1,
+                      max_depth = 5,
+                      gamma = 0, 
+                      min_child_weight = 0,
+                      subsample = 1,
+                      colsample_bytree = 1,
+                      colsample_bylevel = 1,
+                      colsample_bynode = 1,
+                      reg_alpha = 1,
+                      reg_lambda = 1,)
+
+# model = GridSearchCV(xgb, parameters, cv=kfold, n_jobs=8)
 
 model.fit(x_train, y_train, early_stopping_rounds=10, 
           eval_set=[(x_train, y_train), (x_test, y_test)],
-          eval_metric=['logloss'],
-          )
+          eval_metric=['logloss']
+        )
 
-print('테스트 스코어: ', model.score(x_test, y_test))
 
-r2 = r2_score(y_test, model.predict(x_test))
-print('score 결과: ', r2)
+results = model.score(x_test, y_test)
+print('최종 점수 : ', results)
+
+x_predict = model.predict(x_test)
+r2 = r2_score(y_test, x_predict)
+print('진짜 최종 점수 : ', r2)
 
 print(model.feature_importances_)
 
 thresholds = model.feature_importances_
-print('-----------------------------------------------')
+print("=======================================")
+
+##########
 bscore = 0
 idx_ = 0
+##########
+
+# for thresh in thresholds:
 for i in range(len(thresholds)):
     selection = SelectFromModel(model, threshold=thresholds[i], prefit=True)
+    
     select_x_train = selection.transform(x_train)
     select_x_test = selection.transform(x_test)
-    print(select_x_train.shape, select_x_train.shape)
+    print(select_x_train.shape, select_x_test.shape) # (353, 9) (89, 9)
     
-    selection_model = XGBRegressor(n_estimators=100,
-              learning_rate=1,
-              max_depth=2,
-              gamma=0,
-              min_child_weight=1,
-              subsample=1,
-              colsample_bytree=0.5,
-              colsample_bylevel=1,
-              reg_alpha=0.01,
-              colsample_bynode=1,
-              tree_method='gpu_hist', predictor='gpu_predictor', gpu_id=0, random_state=1234,
-              )
+    selection_model = XGBRegressor(n_jobs=-1,
+                                   random_state=123,
+                                   n_estimators=100,
+                                   learning_rate = 0.1,
+                                   max_depth = 3,
+                                   gamma = 1)
     
     selection_model.fit(select_x_train, y_train)
     
     y_predict = selection_model.predict(select_x_test)
     score = r2_score(y_test, y_predict)
-    print('Thresh=%.3f, n=%d, R2: %.2f%%'%(thresholds[i], select_x_train.shape[1], score*100), '\n')
+    
+    print("Thresh=%.3f, n=%d, R2: %.2f%%"
+            %(thresholds[i], select_x_train.shape[1], score*100))
 
     if score >= bscore:
         bscore = score
-        idx_=i
+        idx_= i
 
 f_to_drop = []
 for i in range(len(thresholds)):
@@ -261,25 +143,20 @@ for i in range(len(thresholds)):
         f_to_drop.append(i)
         
 print(f_to_drop)
+# [1, 3, 8]
 
-xaf_train = np.delete(x_train, f_to_drop, axis=1)
-xaf_test = np.delete(x_test, f_to_drop, axis=1)
+x2_train = np.delete(x_train, f_to_drop, axis=1)
+x2_test = np.delete(x_test, f_to_drop, axis=1)
 
-model.fit(xaf_train, y_train)
+model.fit(x2_train, y_train, early_stopping_rounds=10, 
+          eval_set=[(x2_train, y_train), (x2_test, y_test)],
+          )
 
-print('드랍 후 테스트 스코어: ', model.score(xaf_test, y_test))
+print('드랍 후 테스트 스코어: ', model.score(x2_test, y_test))
 
-score = r2_score(y_test, model.predict(xaf_test))
-print('드랍 후 score 결과: ', score)
+score = r2_score(y_test, model.predict(x2_test))
+print('드랍 후 r2_score 결과: ', score)
 
 
-# 테스트 스코어:  0.7254810298421313
-# score 결과:  0.7254810298421313
-# [0.05566699 0.01070472 0.01202314 0.060452   0.3976541  0.
-#  0.18088067 0.04094324 0.04023815 0.20143707 0.         0.        ]
-# -----------------------------------------------
-# (1003, 7) (1003, 7)
-# Thresh=0.040, n=7, R2: 84.28% 
-
-# 드랍 후 테스트 스코어:  0.8183446868464219
-# 드랍 후 score 결과:  0.8183446868464219
+# 드랍 후 테스트 스코어:  0.8939583493278189
+# 드랍 후 r2_score 결과:  0.8939583493278189
