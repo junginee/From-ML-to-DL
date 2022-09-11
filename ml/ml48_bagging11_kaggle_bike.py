19e92abf214b0bb164258b7fb25b1fba320cd494
junginee <107666154+junginee@users.noreply.github.com>
2022-09-12 03:09:54 +0900

    Update ml48_bagging11_kaggle_bike.py

--- ml/ml48_bagging11_kaggle_bike.py
+++ ml/ml48_bagging11_kaggle_bike.py
@@ -15,7 +15,6 @@ test_set = pd.read_csv(path+'test.csv')
 # print(test_set)
 # print(test_set.shape) # (6493, 8)
 
-# datetime 열 내용을 각각 년월일시간날짜로 분리시켜 새 열들로 생성 후 원래 있던 datetime 열을 통째로 drop
 train_set["hour"] = [t.hour for t in pd.DatetimeIndex(train_set.datetime)]
 train_set["day"] = [t.dayofweek for t in pd.DatetimeIndex(train_set.datetime)]
 train_set["month"] = [t.month for t in pd.DatetimeIndex(train_set.datetime)]
@@ -28,18 +27,16 @@ test_set["month"] = [t.month for t in pd.DatetimeIndex(test_set.datetime)]
 test_set['year'] = [t.year for t in pd.DatetimeIndex(test_set.datetime)]
 test_set['year'] = test_set['year'].map({2011:0, 2012:1})
 
-train_set.drop('datetime',axis=1,inplace=True) # train_set에서 데이트타임 드랍
-test_set.drop('datetime',axis=1,inplace=True) # test_set에서 데이트타임 드랍
-train_set.drop('casual',axis=1,inplace=True) # casul 드랍 이유 모르겠음
-train_set.drop('registered',axis=1,inplace=True) # registered 드랍 이유 모르겠음
+train_set.drop('datetime',axis=1,inplace=True) 
+test_set.drop('datetime',axis=1,inplace=True) 
+train_set.drop('casual',axis=1,inplace=True) 
+train_set.drop('registered',axis=1,inplace=True) 
 
 #print(train_set.info())
-# null값이 없으므로 결측치 삭제과정 생략
 
 x = train_set.drop(['count'], axis=1)
 y = train_set['count']
 x = np.array(x)
-
 
 x_train, x_test, y_train, y_test = train_test_split(
     x, y, random_state=66, train_size=0.8, shuffle=True)
