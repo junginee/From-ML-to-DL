c7c29cb89a83c24e8a64b7e45f1566c7a866f94f
junginee <107666154+junginee@users.noreply.github.com>
2022-09-12 03:10:31 +0900

    Update ml48_bagging12_kaggle_house.py

--- ml/ml48_bagging12_kaggle_house.py
+++ ml/ml48_bagging12_kaggle_house.py
@@ -43,16 +43,14 @@ for col in tqdm_notebook(cols):
 
 
 #### 결측치  제거 ####
-print(train_set.isnull().sum()) # 각 컬럼당 null의 갯수 확인가능
-train_set = train_set.fillna(train_set.mean()) # nan 값을 채우거나(fillna) 행별로 모두 삭제(dropna)
+print(train_set.isnull().sum()) 
+train_set = train_set.fillna(train_set.mean()) 
 print(train_set.isnull().sum())
-print(train_set.shape) # (1460, 80) 데이터가 얼마나 삭제된 것인지 확인가능(1460-1460=0)
- 
+print(train_set.shape) 
 
 test_set = test_set.fillna(test_set.mean())
 
-
-x = train_set.drop(['SalePrice'], axis=1) # axis는 'count'가 컬럼이라는 것을 명시하기 위해
+x = train_set.drop(['SalePrice'], axis=1) 
 print(x)
 print(x.columns)
 print(x.shape) # (1460, 79)
