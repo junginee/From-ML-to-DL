4a016098fd4c7c87bfac192840f39614bb7730ad
junginee <107666154+junginee@users.noreply.github.com>
2022-09-15 09:41:19 +0900

    Update ml51_KMeans3_cancer.py

--- ml/ml51_KMeans3_cancer.py
+++ ml/ml51_KMeans3_cancer.py
@@ -1,9 +1,5 @@
 import numpy as np
 import pandas as pd
-
-from sklearn.ensemble import VotingClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.datasets import load_breast_cancer
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
@@ -14,7 +10,6 @@ warnings.filterwarnings("ignore")
 
 datasets = load_breast_cancer()
 print(np.unique(datasets.target)) #[0 1]
-
 
 df = pd.DataFrame(datasets.data, columns=[datasets.feature_names])
 print(df)
@@ -30,7 +25,7 @@ print(datasets.target) #데이터셋에 들어있는 target
 df['cluster'] = kmeans.labels_
 df['target'] = datasets.target
 
-# accuracy_score 구하기
+# accuracy_score 
 score =  accuracy_score(kmeans.labels_,datasets.target )
 print('score :', round(score,5))
 
