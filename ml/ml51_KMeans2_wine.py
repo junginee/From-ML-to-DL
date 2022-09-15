778f29f9878f9e336c5b0eff55e6e02f9d5318ec
junginee <107666154+junginee@users.noreply.github.com>
2022-09-15 09:40:08 +0900

    Update ml51_KMeans2_wine.py

--- ml/ml51_KMeans2_wine.py
+++ ml/ml51_KMeans2_wine.py
@@ -1,9 +1,5 @@
 import numpy as np
 import pandas as pd
-
-from sklearn.ensemble import VotingClassifier
-from sklearn.linear_model import LogisticRegression
-from sklearn.neighbors import KNeighborsClassifier
 from sklearn.datasets import load_wine
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
@@ -15,17 +11,15 @@ warnings.filterwarnings("ignore")
 datasets = load_wine()
 print(np.unique(datasets.target)) #[0 1 2]
 
-
 df = pd.DataFrame(datasets.data, columns=[datasets.feature_names])
 print(df)
 
 kmeans = KMeans(n_clusters=3, random_state=1234)
 #n_clusters = y 라벨의 갯수(0,1,2)
 
-kmeans.fit(df) # x를 훈련시켜 y(라벨)을 찾는 방식
-
-print(kmeans.labels_) #훈련을 통해 찾은 y라벨
-print(datasets.target) #데이터셋에 들어있는 target
+kmeans.fit(df)
+print(kmeans.labels_)
+print(datasets.target) 
 
 df['cluster'] = kmeans.labels_
 df['target'] = datasets.target
