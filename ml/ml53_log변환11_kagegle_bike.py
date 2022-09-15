913e31f18274b1ad6f701c811e82f33b558c6d82
junginee <107666154+junginee@users.noreply.github.com>
2022-09-15 17:12:56 +0900

    Update ml53_log변환11_kagegle_bike.py

--- ml/ml53_log변환11_kagegle_bike.py
+++ ml/ml53_log변환11_kagegle_bike.py
@@ -9,8 +9,6 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from xgboost import XGBClassifier, XGBRegressor 
-from sklearn.pipeline import make_pipeline
-import matplotlib.pyplot as plt
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -37,10 +35,10 @@ test_set["month"] = [t.month for t in pd.DatetimeIndex(test_set.datetime)]
 test_set['year'] = [t.year for t in pd.DatetimeIndex(test_set.datetime)]
 test_set['year'] = test_set['year'].map({2011:0, 2012:1})
 
-train_set.drop('datetime',axis=1,inplace=True) # train_set에서 데이트타임 드랍
-test_set.drop('datetime',axis=1,inplace=True) # test_set에서 데이트타임 드랍
-train_set.drop('casual',axis=1,inplace=True) # casul 드랍 이유 모르겠음
-train_set.drop('registered',axis=1,inplace=True) # registered 드랍 이유 모르겠음
+train_set.drop('datetime',axis=1,inplace=True)
+test_set.drop('datetime',axis=1,inplace=True)
+train_set.drop('casual',axis=1,inplace=True)
+train_set.drop('registered',axis=1,inplace=True)
 
 #print(train_set.info())
 # null값이 없으므로 결측치 삭제과정 생략
@@ -49,11 +47,9 @@ x = train_set.drop(['count'], axis=1)
 y = train_set['count']
 x = np.array(x)
 
-
 x_train, x_test, y_train, y_test = train_test_split(
     x,y, test_size = 0.2, random_state=1234,
 )
-
 
 scaler = StandardScaler()
 x_train = scaler.fit_transform(x_train)
