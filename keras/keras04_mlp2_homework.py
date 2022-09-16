6d48b4ba5a4c7c433bad31e4f1fc6785e18a310c
junginee <107666154+junginee@users.noreply.github.com>
2022-09-17 00:02:40 +0900

    Update keras04_mlp2_homework.py

--- keras/keras04_mlp2_homework.py
+++ keras/keras04_mlp2_homework.py
@@ -6,7 +6,7 @@ from tensorflow.keras.layers import Dense
 x = np.array([[1,2,3,4,5,6,7,8,9,10],
              [1, 1, 1, 1, 2, 1.3, 1.4, 1.5, 1.6, 1.4],
              [9,8,7,6,5,4,3,2,1,0]]
-            )
+            )          
 y = np.array([11,12,13,14,15,16,17,18,19,20])   
 print(x.shape)  #(3,10)
 print(y.shape)  #(10,)
