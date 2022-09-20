866cb1bc44bd5fbeb737b251a44018721f053f81
junginee <107666154+junginee@users.noreply.github.com>
2022-09-20 10:05:41 +0900

    Update keras09_verbose.py

--- keras/keras09_verbose.py
+++ keras/keras09_verbose.py
@@ -1,7 +1,3 @@
-#verbose ; 훈련과정 생략 > 장점 : 빠르게 처리할 수 있다. (시간 지체 현상이 적게 나타난다.)
-#verbose를 0으로 하고(훈련과정이 출력되는 것을 생략하고), 훈련시간이 얼마나 걸렸는지 확인해보자!  >> (1)import time (2)start_time = time.time() (3)print(start_time)
-                                                                                             #(4)end_time = time.time() - start_time
-
 import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
@@ -39,10 +35,3 @@ model.fit(x_train, y_train, epochs=50, batch_size=7, verbose=0)
 end_time = time.time() - start_time
 
 print("걸린시간 : ", end_time)
-
-"""
-vervose 0 걸린시간 : 출력없다. (진행상황을 볼 수 없다.)
-vervose 1 걸린시간 : 잔소리많다.
-vervose 2 걸린시간 : 프로그래스바 없다.
-vervose 3,4,5... 걸린시간 : epoch만 나온다. 
-"""  
