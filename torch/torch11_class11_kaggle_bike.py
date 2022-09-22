6cde6f0a61c937f6610aa3d04207445765239587
junginee <107666154+junginee@users.noreply.github.com>
2022-09-22 20:41:36 +0900

    Update torch11_class11_kaggle_bike.py

---
+++ torch/torch11_class11_kaggle_bike.py
@@ -0,0 +1,146 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_squared_error
+import datetime as dt
+from sklearn.preprocessing import MinMaxScaler,StandardScaler,MaxAbsScaler,RobustScaler
+import datetime
+     
+#1. 데이터
+path = 'D:\study_data\_data\kaggle_bike/'
+train_set = pd.read_csv(path + 'train.csv') 
+            
+test_set = pd.read_csv(path + 'test.csv') 
+
+train_set["hour"] = [t.hour for t in pd.DatetimeIndex(train_set.datetime)]
+train_set["day"] = [t.dayofweek for t in pd.DatetimeIndex(train_set.datetime)]
+train_set["month"] = [t.month for t in pd.DatetimeIndex(train_set.datetime)]
+train_set['year'] = [t.year for t in pd.DatetimeIndex(train_set.datetime)]
+train_set['year'] = train_set['year'].map({2011:0, 2012:1})
+
+test_set["hour"] = [t.hour for t in pd.DatetimeIndex(test_set.datetime)]
+test_set["day"] = [t.dayofweek for t in pd.DatetimeIndex(test_set.datetime)]
+test_set["month"] = [t.month for t in pd.DatetimeIndex(test_set.datetime)]
+test_set['year'] = [t.year for t in pd.DatetimeIndex(test_set.datetime)]
+test_set['year'] = test_set['year'].map({2011:0, 2012:1})
+
+train_set.drop('datetime',axis=1,inplace=True) 
+train_set.drop('casual',axis=1,inplace=True)
+train_set.drop('registered',axis=1,inplace=True)
+
+test_set.drop('datetime',axis=1,inplace=True) 
+
+
+x = train_set.drop(['count'], axis=1)  
+print(x)
+print(x.columns)
+print(x.shape) # (10886, 12)
+
+y = train_set['count'] 
+
+import torch 
+import torch.nn as nn 
+import torch.optim as optim 
+import torch.nn.functional as F
+ 
+USE_CUDA = torch.cuda.is_available
+DEVICE = torch.device('cuda:0' if USE_CUDA else 'cpu')
+print('torch : ', torch.__version__, '사용DEVICE : ', DEVICE)
+
+x = x.to_numpy()
+y = y.to_numpy()
+
+x = torch.FloatTensor(x)
+y = torch.FloatTensor(y)
+
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(x,y, train_size=0.8,shuffle=True, random_state=123)
+
+x_train = torch.FloatTensor(x_train)
+y_train = torch.FloatTensor(y_train).unsqueeze(1).to(DEVICE)
+x_test = torch.FloatTensor(x_test)
+y_test = torch.FloatTensor(y_test).unsqueeze(1).to(DEVICE)
+
+print(x_train.shape,y_train.shape,x_test.shape,y_test.shape)
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+
+x_train = torch.FloatTensor(x_train).to(DEVICE)
+x_test = torch.FloatTensor(x_test).to(DEVICE)
+
+#2.모델 
+# model = nn.Sequential(
+#     nn.Linear(12,64),
+#     nn.ReLU(),
+#     nn.Linear(64,32),
+#     nn.ReLU(),
+#     nn.Linear(32,16),
+#     nn.ReLU(),
+#     nn.Linear(16,1),
+      
+# ).to(DEVICE)
+
+
+class Model(nn.Module):
+    def __init__(self,input_dim,output_dim):
+        super().__init__()
+        self.L1 = nn.Linear(input_dim, 64)
+        self.L2 = nn.Linear(64, 32)
+        self.L3 = nn.Linear(32, 16)
+        self.L4 = nn.Linear(16, 1)
+        self.relu = nn.ReLU()
+        
+    def forward(self,input_size):
+        x = self.L1(input_size)
+        x = self.relu(x)
+        x = self.L2(x)
+        x = self.relu(x)
+        x = self.L3(x)
+        x = self.relu(x)
+        x = self.L4(x)
+        return x 
+
+model = Model(12,1).to(DEVICE)
+
+#3.컴파일,훈련 
+criterion = nn.MSELoss()  
+optimizer = optim.Adam(model.parameters(),lr=0.01)
+
+def train(model,criterion,optimizer,x_train,y_train):
+    optimizer.zero_grad()
+    
+    hypothesis = model(x_train)
+    
+    loss = criterion(hypothesis,y_train)
+    
+    loss.backward()
+    optimizer.step()
+    return loss.item()
+
+epochs = 2000
+for epoch in range(epochs+1):
+    loss = train(model,criterion,optimizer,x_train,y_train)
+    print('epochs : {}, loss : {}'.format(epochs,loss))
+    
+#4.평가,예측
+def evaluate(model,criterion,x_test,y_test):
+    model.eval()
+    
+    with torch.no_grad():
+        y_predict = model(x_test)
+        results = criterion(y_predict,y_test)    
+    return results.item()
+
+loss2 = evaluate(model,criterion,x_test,y_test)
+print('최종 loss : ', loss2)
+from sklearn.metrics import accuracy_score,r2_score
+y_predict = model(x_test)
+score = r2_score(y_predict.cpu().detach(),y_test.cpu().detach())
+print('r2 : ', score)
+
+# 최종 loss :  1684.18798828125
+# r2 :  0.9457936228888987
