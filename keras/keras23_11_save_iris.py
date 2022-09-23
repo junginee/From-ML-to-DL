75ab5440878b6dfcea97eca3a665a4e1622b4b51
junginee <107666154+junginee@users.noreply.github.com>
2022-09-23 10:16:30 +0900

    Update keras23_11_save_iris.py

--- keras/keras23_11_save_iris.py
+++ keras/keras23_11_save_iris.py
@@ -1,21 +1,16 @@
-
-
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.datasets import load_iris 
 from sklearn.model_selection import train_test_split 
-
 from tensorflow.python.keras.models import Sequential, load_model
 from tensorflow.python.keras.layers import Dense 
-
 from sklearn.metrics import accuracy_score 
 import time
-
 import tensorflow as tf
 tf.random.set_seed(66)
 
-#1. 데이터
 
+#1. 데이터
 datasets = load_iris()
 # print(datasets.DESCR)
 # print(datasets.feature_names)
@@ -37,7 +32,6 @@ print(y.shape) #(150,3)
 
 x_train, x_test, y_train, y_test = train_test_split( x, y, train_size = 0.8, shuffle=True, random_state=68 )
 
-
 scaler = MinMaxScaler()
 scaler.fit(x_train)
 x_train = scaler.transform(x_train) 
@@ -45,8 +39,8 @@ x_test = scaler.transform(x_test)
 print(np.min(x_train)) #0.0 
 print(np.max(x_train)) #1.0
 
-#2. 모델구성
 
+#2. 모델구성
 model = Sequential()
 model.add(Dense(5,input_dim = 4))
 model.add(Dense(10, activation='relu'))
@@ -55,9 +49,7 @@ model.add(Dense(20, activation='relu'))
 model.add(Dense(3, activation='softmax'))  
 
 
-
 #3. 컴파일, 훈련
-
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # 이진분류모델에서는 loss로 binary_crossentropy
@@ -74,13 +66,11 @@ hist = model.fit(x_train, y_train, epochs=100, batch_size=50,
 
 model.save_weights("./_save/keras23_11_save_iris.h5")
 
-#4. 평가, 예측
 
+#4. 평가, 예측
 results = model.evaluate(x_test, y_test)
 print('loss : ' , results[0])
 print('accuracy : ', results[1]) 
-
-
 
 print("----------------------------------------")
 
