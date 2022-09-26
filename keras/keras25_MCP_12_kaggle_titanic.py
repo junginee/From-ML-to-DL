c09cae0bf008184bf2239241367681a34de68321
junginee <107666154+junginee@users.noreply.github.com>
2022-09-26 18:32:54 +0900

    Update keras25_MCP_12_kaggle_titanic.py

--- keras/keras25_MCP_12_kaggle_titanic.py
+++ keras/keras25_MCP_12_kaggle_titanic.py
@@ -21,19 +21,19 @@ print(a)
 print(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))) #현재 파일이 위치한 경로
 print(a.split("\\")[-1]) #현재 파일 명
 current_name = a.split("\\")[-1]
-##########################밑에 filepath경로에 추가로  + current_name + '/' 삽입해야 돌아감#######################
+##########################밑에 filepath경로에 추가로  + current_name + '/' 삽입해야 돌아감###################
 
 
 #1. 데이터
 path = './_data/kaggle_titanic/'
-train_set = pd.read_csv(path + 'train.csv', # + 명령어는 문자를 앞문자와 더해줌
-                        index_col=0) # index_col=n n번째 컬럼을 인덱스로 인식
+train_set = pd.read_csv(path + 'train.csv',
+                        index_col=0)
 # print(train_set)
 # print(train_set.shape) # (891, 11)
 # print(train_set.describe())
 # print(train_set.columns)
 
-test_set = pd.read_csv(path + 'test.csv', # 예측에서 쓸거임                
+test_set = pd.read_csv(path + 'test.csv',                 
                        index_col=0)
 # print(test_set)
 # print(test_set.shape) # (418, 10)
@@ -55,14 +55,11 @@ print(f"Percentage of males who survived: {male}")
 
 sns.barplot(x="SibSp", y="Survived", data=train_set)
 
-
 # df = pd.DataFrame(y)
 # print(df)
 # oh = OneHotEncoder(sparse=False) # sparse=true 는 매트릭스반환 False는 array 반환
 # y = oh.fit_transform(df)
 # print(y)
-
-
 
 # print(test_set.columns)
 # print(train_set.info()) # info 정보출력
@@ -92,8 +89,7 @@ print(train_set, test_set, train_set.shape, test_set.shape)
 
 ############################
 
-
-x = train_set.drop(['Survived'], axis=1)  # drop 데이터에서 ''사이 값 빼기
+x = train_set.drop(['Survived'], axis=1)  
 print(x)
 print(x.columns)
 print(x.shape) # (891, 8)
@@ -123,21 +119,7 @@ print(np.max(x_test))  # 1.0
 
 
 #2. 모델구성
-
 # model = load_model("./_save/keras22_hamsu12_kaggle_titanic.h5")
-
-# model = Sequential()
-# model.add(Dense(300, input_dim=8, activation='relu')) #sigmoid : 이진분류일때 아웃풋에 activation = 'sigmoid' 라고 넣어줘서 아웃풋 값 범위를 0에서 1로 제한해줌
-# model.add(Dense(200, activation='relu'))               # 출력이 0 or 1으로 나와야되기 때문, 그리고 최종으로 나온 값에 반올림을 해주면 0 or 1 완성
-# model.add(Dense(200, activation='relu'))               # relu : 히든에서만 쓸수있음, 요즘에 성능 젤좋음
-# model.add(Dense(200, activation='relu'))
-# model.add(Dense(200, activation='relu'))
-# model.add(Dense(200, activation='relu'))
-# model.add(Dense(200, activation='relu'))
-# model.add(Dense(200, activation='relu'))
-# model.add(Dense(200, activation='relu'))
-# model.add(Dense(200, activation='relu'))               
-# model.add(Dense(1, activation='sigmoid'))   
 
 input1 = Input(shape=(8,))
 dense1 = Dense(300, activation='relu')(input1)
@@ -155,8 +137,7 @@ model = Model(inputs=input1, outputs=output1)
                                                                         
 #3. 컴파일, 훈련
 model.compile(loss='binary_crossentropy', optimizer='adam',
-              metrics=['accuracy'])   # 이진분류에 한해 로스함수는 무조건 99퍼센트로 'binary_crossentropy'
-                                      # 컴파일에있는 metrics는 평가지표라고도 읽힘
+              metrics=['accuracy'])   
 
 from tensorflow.python.keras.callbacks import EarlyStopping, ModelCheckpoint
 import datetime
@@ -186,30 +167,6 @@ y_predict = model.predict(x_test)
 print(y_predict)
 y_predict = y_predict.round(0)
 print(y_predict)
-
-
-# y_summit = model.predict(test_set)
-
-# print(y_summit)
-# print(y_summit.shape) # (418, 1)
-# y_summit = y_summit.round()
-# df = pd.DataFrame(y_summit)
-# print(df)
-# oh = OneHotEncoder(sparse=False) # sparse=true 는 매트릭스반환 False는 array 반환
-# y_summit = oh.fit_transform(df)
-# print(y_summit)
-# y_summit = np.argmax(y_summit, axis= 1)
-# submission_set = pd.read_csv(path + 'gender_submission.csv', # + 명령어는 문자를 앞문자와 더해줌
-#                              index_col=0) # index_col=n n번째 컬럼을 인덱스로 인식
-
-# print(submission_set)
-
-# submission_set['Survived'] = y_summit
-# print(submission_set)
-
-
-# submission_set.to_csv(path + 'submission_maxabs.csv', index = True)
-
 
 acc= accuracy_score(y_test, y_predict)
 print('loss : ' , loss)
