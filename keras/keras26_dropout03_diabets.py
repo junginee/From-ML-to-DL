03b0ea1fd38b376ca2662111e0fe5e50ef4d1eff
junginee <107666154+junginee@users.noreply.github.com>
2022-09-26 18:40:18 +0900

    Update keras26_dropout03_diabets.py

--- keras/keras26_dropout03_diabets.py
+++ keras/keras26_dropout03_diabets.py
@@ -1,85 +1,82 @@
-from sklearn.model_selection import train_test_split
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Dense, Dropout
-import matplotlib.pyplot as plt
-from matplotlib import font_manager, rc
-font_path = "C:/Windows/Fonts/gulim.TTc"
-font = font_manager.FontProperties(fname=font_path).get_name()
-rc('font', family=font)
-from sklearn.datasets import load_diabetes
-import time
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
-from sklearn.preprocessing import MaxAbsScaler, RobustScaler
 import numpy as np
+from sklearn import datasets
+from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MaxAbsScaler, MinMaxScaler, StandardScaler, RobustScaler
+from tensorflow.python.keras.models import Sequential, Model, load_model
+from tensorflow.python.keras.layers import Dense, Input, Dropout
+from tensorflow.python.keras.callbacks import EarlyStopping, ModelCheckpoint
+from sklearn.metrics import r2_score, accuracy_score
+import time
 
 
 #1. 데이터
 datasets = load_diabetes()
-x = datasets.data
-y = datasets.target
-
-x_train, x_test, y_train, y_test = train_test_split(x,y,
-                                                    train_size=0.8,
-                                                    random_state=72
-                                                    )
-
-# scaler = MinMaxScaler()
-# scaler = StandardScaler()
-scaler = MaxAbsScaler()
-# scaler = RobustScaler()
-scaler.fit(x_train)
-x_train = scaler.transform(x_train)
-x_test = scaler.transform(x_test)
-print(np.min(x_train))  # 0.0
-print(np.max(x_train))  # 1.0
-
-print(np.min(x_test))  # 1.0
-print(np.max(x_test))  # 1.0
+x, y = datasets.data, datasets.target
 
 
-#2. 모델구성
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, train_size=0.7, random_state=66
+)
+
+
+#2. 모델 구성
 model = Sequential()
-model.add(Dense(200, input_dim=10))
-model.add(Dense(300))
-model.add(Dropout(0.3))
-model.add(Dense(200))
-model.add(Dense(300,activation='relu'))
-model.add(Dense(150))
-model.add(Dense(180))
-model.add(Dropout(0.3))
-model.add(Dense(1))
+model.add(Dense(100, activation='linear', input_dim=10))
+model.add(Dense(100, activation='relu'))
+model.add(Dropout(0.3))                                     # 연산 때 30% 비율로 랜덤하게 노드를 없앰
+model.add(Dense(100, activation='relu'))
+model.add(Dense(100, activation='relu'))
+model.add(Dropout(0.2))                                     # 연산 때 20% 비율로 랜덤하게 노드를 없앰
+model.add(Dense(100, activation='relu'))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(1, activation='linear'))
 
-#3. 컴파일, 훈련
-model.compile(loss='mse', optimizer='adam', metrics=['mae'])
 
-from tensorflow.python.keras.callbacks import EarlyStopping
-earlyStopping = EarlyStopping(monitor='val_loss', patience=100, mode='min', verbose=1, 
+#3. 훈련
+model.compile(loss='mse', optimizer='adam', metrics=['mse'])  
+
+import datetime
+date = datetime.datetime.now()      # 2022-07-07 17:21:42.275191
+date = date.strftime("%m%d_%H%M")   # 0707_1723
+print(date)
+
+filepath = './_ModelCheckPoint/k26/'
+filename = '{epoch:04d}-{val_loss:.4f}.hdf5'
+
+earlyStopping = EarlyStopping(monitor = 'val_loss', patience=100, mode='min', 
+                              verbose=1, 
                               restore_best_weights=True)
+mcp = ModelCheckpoint(monitor='val_loss', mode='auto', verbose=1, 
+                      save_best_only=True, 
+                      filepath="".join([filepath, '03_', date, '_', filename])
+                      )
 
-hist = model.fit(x_train, y_train, epochs=500, batch_size=10,verbose=1,validation_split=0.2, callbacks=[earlyStopping])
-print(hist)
+start_time = time.time() 
+hist = model.fit(x_train, y_train, epochs=1000, batch_size=100,
+                 validation_split=0.2,
+                 callbacks=[earlyStopping, mcp],
+                 verbose=1)
+end_time = time.time() - start_time
 
-
-end_time = time.time()
 
 #4. 평가, 예측
 loss = model.evaluate(x_test, y_test)
-y_predict = model.predict(x_test)
+print('loss : ', loss)
 
-
-
-print("걸린시간 : ", end_time)
-from sklearn.metrics import r2_score
+y_predict = model.predict(x_test)  
 r2 = r2_score(y_test, y_predict)
-print('loss : ' , loss)
-print('r2스코어 : ', r2)
+print('r2 스코어: ', r2)  
 
-#drop out 이전
-# 걸린시간 :  1657245923.682995
-# loss :  [2132.64697265625, 37.838951110839844]
-# r2스코어 :  0.6770291455619224
 
-#drop out 이후
-# 걸린시간 :  1657246221.6489372
-# loss :  [2171.82666015625, 37.63895034790039]
-# r2스코어 :  0.6710957536220732 
+#================================= 1. 기본 출력 ===================================#
+# loss :  [3216.153076171875, 3216.153076171875]
+# r2 스코어:  0.4837942008323731
+# _03_0707_1918_0028-2992.4802.hdf5
+#=================================================================================#
+
+#================================ 2. dorpout 적용 ================================#
+# loss :  [3145.81201171875, 3145.81201171875]
+# r2 스코어:  0.4950842050835256
+# _03_0708_1056_0228-2951.7988.hdf5
+#=================================================================================#
