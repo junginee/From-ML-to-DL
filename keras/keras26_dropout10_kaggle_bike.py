7310821cf5141f74ebf55380a689df01bb449e1b
junginee <107666154+junginee@users.noreply.github.com>
2022-09-26 18:47:58 +0900

    Update keras26_dropout10_kaggle_bike.py

--- keras/keras26_dropout10_kaggle_bike.py
+++ keras/keras26_dropout10_kaggle_bike.py
@@ -1,44 +1,20 @@
 import numpy as np
 import pandas as pd
-from sqlalchemy import true #pandas : 엑셀땡겨올때 씀
-from tensorflow.python.keras.models import Sequential, Model
-from tensorflow.python.keras.layers import Dense, Input
+from tensorflow.python.keras.models import Sequential,Model
+from tensorflow.python.keras.layers import Dense,Input,Dropout
 from keras.layers.recurrent import LSTM, SimpleRNN
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
-from sklearn.preprocessing import MaxAbsScaler, RobustScaler
 import datetime as dt
-
-###########################폴더 생성시 현재 파일명으로 자동생성###########################################
-import inspect, os
-a = inspect.getfile(inspect.currentframe()) #현재 파일이 위치한 경로 + 현재 파일 명
-print(a)
-print(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))) #현재 파일이 위치한 경로
-print(a.split("\\")[-1]) #현재 파일 명
-current_name = a.split("\\")[-1]
-##########################밑에 filepath경로에 추가로  + current_name + '/' 삽입해야 돌아감#######################
-
+from sklearn.preprocessing import MinMaxScaler,StandardScaler,MaxAbsScaler,RobustScaler
+from tensorflow.python.keras.callbacks  import EarlyStopping,ModelCheckpoint
+import datetime
+     
 #1. 데이터
 path = './_data/kaggle_bike/'
-train_set = pd.read_csv(path + 'train.csv') # + 명령어는 문자를 앞문자와 더해줌  index_col=n n번째 컬럼을 인덱스로 인식
+train_set = pd.read_csv(path + 'train.csv') 
             
-test_set = pd.read_csv(path + 'test.csv') # 예측에서 쓸거임        
-
-'''                        
-print(train_set)
-print(train_set.shape) # (10886, 12)
-                  
-print(test_set)
-print(test_set.shape) # (6493, 9)
-print(test_set.info()) # (715, 9)
-print(train_set.columns)
-print(train_set.info()) # info 정보출력
-print(train_set.describe()) # describe 평균치, 중간값, 최소값 등등 출력
-'''
-
-
-######## 년, 월 ,일 ,시간 분리 ############
+test_set = pd.read_csv(path + 'test.csv') 
 
 train_set["hour"] = [t.hour for t in pd.DatetimeIndex(train_set.datetime)]
 train_set["day"] = [t.dayofweek for t in pd.DatetimeIndex(train_set.datetime)]
@@ -52,18 +28,14 @@ test_set["month"] = [t.month for t in pd.DatetimeIndex(test_set.datetime)]
 test_set['year'] = [t.year for t in pd.DatetimeIndex(test_set.datetime)]
 test_set['year'] = test_set['year'].map({2011:0, 2012:1})
 
-train_set.drop('datetime',axis=1,inplace=True) # 트레인 세트에서 데이트타임 드랍
-train_set.drop('casual',axis=1,inplace=True) # 트레인 세트에서 캐주얼 레지스터드 드랍
+train_set.drop('datetime',axis=1,inplace=True) 
+train_set.drop('casual',axis=1,inplace=True)
 train_set.drop('registered',axis=1,inplace=True)
 
-test_set.drop('datetime',axis=1,inplace=True) # 트레인 세트에서 데이트타임 드랍
-
-print(train_set)
-print(test_set)
-##########################################
+test_set.drop('datetime',axis=1,inplace=True) 
 
 
-x = train_set.drop(['count'], axis=1)  # drop 데이터에서 ''사이 값 빼기
+x = train_set.drop(['count'], axis=1)  
 print(x)
 print(x.columns)
 print(x.shape) # (10886, 12)
@@ -72,66 +44,42 @@ y = train_set['count']
 print(y)
 print(y.shape) # (10886,)
 
-x_train, x_test, y_train, y_test = train_test_split(x,y,
-                                                    train_size=0.75,
-                                                    random_state=31
-                                                    )
+x_train, x_test, y_train, y_test = train_test_split(x,y,train_size=0.75,random_state=31)   
+      
 
-# scaler = MinMaxScaler()
 scaler = StandardScaler()
-# scaler = MaxAbsScaler()
-# scaler = RobustScaler()
-scaler.fit(x_train)
-x_train = scaler.transform(x_train)
-x_test = scaler.transform(x_test)
-test_set = scaler.transform(test_set)
 
-
-
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test) 
+test_set = scaler.transform(test_set)      
+                                                    
 
 #2. 모델구성
+model = Sequential()
+model.add(Dense(100, activation='relu', input_dim=12))
+model.add(Dense(100, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(100, activation='relu'))
+model.add(Dropout(0.3))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(1))
 
-# model = load_model("./_save/keras22_hamsu10_kaggle_bike.h5")
-
-# model = Sequential()
-# model.add(Dense(100, activation='swish', input_dim=12))
-# model.add(Dense(100, activation='elu'))
-# model.add(Dense(100, activation='swish'))
-# model.add(Dense(100, activation='elu'))
-# model.add(Dense(1))
-
-input1 = Input(shape=(12,))
-dense1 = Dense(100, activation='swish')(input1)
-dense2 = Dense(100, activation='elu')(dense1)
-dense3 = Dense(100, activation='swish')(dense2)
-dense4 = Dense(100, activation='elu')(dense3)
-output1 = Dense(1)(dense4)
-model = Model(inputs=input1, outputs=output1)     
-
+date=datetime.datetime.now()
+date=date.strftime('%m%d_%H%M')
 
 #3. 컴파일, 훈련
-model.compile(loss='mse', optimizer='adam', metrics=['mae'])
-from tensorflow.python.keras.callbacks import EarlyStopping, ModelCheckpoint
-import datetime
-date = datetime.datetime.now()
-date = date.strftime("%m%d_%H%M") # 0707_1723
-print(date)
+model.compile(loss='mse', optimizer='adam')
+     
 
-filepath = './_ModelCheckPoint/' + current_name + '/'
-filename = '{epoch:04d}-{val_loss:.4f}.hdf5'
+earlyStopping =EarlyStopping(monitor = 'val_loss',patience=30,mode='min',restore_best_weights=True,verbose=1)
 
-earlyStopping = EarlyStopping(monitor='val_loss', patience=100, mode='auto', verbose=1, 
-                              restore_best_weights=True)        
+# filepath='./_ModelCheckpoint/k24/'
+# filename='{epoch:04d}-{val_loss:.4f}.hdf5'
 
-mcp = ModelCheckpoint(monitor='val_loss', mode='auto', verbose=1, save_best_only=True, 
-                      filepath= "".join([filepath, date, '_', filename])
-                      )
+# mcp = ModelCheckpoint(monitor = 'val_loss',mode = 'auto', save_best_only=True, verbose=1,
+#                       filepath="".join([filepath,'kaggle_bike',date,'_',filename]))
 
-hist = model.fit(x_train, y_train, epochs=1000, batch_size=100,
-                 validation_split=0.2,
-                 callbacks=[earlyStopping, mcp],
-                 verbose=1)
-
+model.fit(x_train, y_train, epochs=800, batch_size=60,validation_split=0.2,callbacks=[earlyStopping], verbose=2)
 
 #4. 평가, 예측
 loss = model.evaluate(x, y) 
@@ -139,37 +87,12 @@ print('loss : ', loss)
 
 y_predict = model.predict(x_test)
 
+
 def RMSE(a, b): 
     return np.sqrt(mean_squared_error(a, b))
 
 rmse = RMSE(y_test, y_predict)
-
-from sklearn.metrics import r2_score
-r2 = r2_score(y_test, y_predict)
-
-print('loss : ', loss)
 print("RMSE : ", rmse)
-print('r2스코어 : ', r2)
 
-# y_summit = model.predict(test_set)
-
-# print(y_summit)
-# print(y_summit.shape) # (6493, 1)
-
-# submission_set = pd.read_csv(path + 'sampleSubmission.csv', # + 명령어는 문자를 앞문자와 더해줌
-#                              index_col=0) # index_col=n n번째 컬럼을 인덱스로 인식
-
-# print(submission_set)
-
-# submission_set['count'] = y_summit
-# print(submission_set)
-
-
-# submission_set.to_csv(path + 'submission_robust_scaler.csv', index = True)
-
-
-#drop out 이전
-
-
-
-#drop out 이후
+# loss :  2176.55322265625
+# RMSE :  53.909264740796814
