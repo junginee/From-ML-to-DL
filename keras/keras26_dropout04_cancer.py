7141367671d00f40dce939447aea730b49d0bfc3
junginee <107666154+junginee@users.noreply.github.com>
2022-09-26 18:40:48 +0900

    Update keras26_dropout04_cancer.py

--- keras/keras26_dropout04_cancer.py
+++ keras/keras26_dropout04_cancer.py
@@ -1,113 +1,96 @@
 import numpy as np
-import pandas as pd
-from sklearn.datasets import load_iris
-from sqlalchemy import false
-from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Dense
+from sklearn import datasets
+from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
-from tensorflow.python.keras.callbacks import EarlyStopping
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, MaxAbsScaler, RobustScaler
+from tensorflow.python.keras.models import Sequential, load_model
+from tensorflow.python.keras.layers import Dense, Dropout
+from tensorflow.python.keras.callbacks import EarlyStopping, ModelCheckpoint
 from sklearn.metrics import r2_score, accuracy_score
-from tensorflow.keras.utils import to_categorical # https://wikidocs.net/22647 케라스 원핫인코딩
-from sklearn.preprocessing import OneHotEncoder  # https://psystat.tistory.com/136 싸이킷런 원핫인코딩
-from sklearn.datasets import fetch_covtype
-import tensorflow as tf
+import time
 
 #1. 데이터
+datasets = load_breast_cancer()
+x, y = datasets.data, datasets.target
 
-datasets = fetch_covtype()
-x = datasets.data
-y = datasets.target
-print(x.shape, y.shape) # (581012, 54) (581012,)
-print(np.unique(y)) # [1 2 3 4 5 6 7]
-# print(x,y)
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, train_size=0.7, random_state=66
+)
 
-# print(datasets.DESCR)
-# print(datasets.feature_names)
-# print(datasets)
-
-# print(x)
-# print(y)
-####################케라스########################
-# y = to_categorical(y)
-# print(np.unique(y, return_counts=True)) # y의 라벨값 :  [1 2 3 4 5 6 7]
-#################################################
-
-####################겟더미#######################
-# y = pd.get_dummies(y)  #겟더미는 y_predict 할때 np아니고 tf.argmax로 바꾸기
-# print(y)
-################################################
-
-####################원핫인코더###################
-df = pd.DataFrame(y)
-print(df)
-oh = OneHotEncoder(sparse=False) # sparse=true 는 매트릭스반환 False는 array 반환
-y = oh.fit_transform(df)
-print(y)
-################################################
+scaler = RobustScaler()
+scaler.fit(x_train)
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test)
 
 
-x_train, x_test, y_train, y_test = train_test_split(x,y,
-                                                    train_size=0.7,
-                                                    random_state=66
-                                                    )
-
-print(y_test)
-print(y_train)
-print(y)
-
-
-#2. 모델
-
+#2. 모델 구성
 model = Sequential()
-model.add(Dense(30, input_dim=54, activation='relu')) 
-model.add(Dense(20, activation='sigmoid'))               
-model.add(Dense(20, activation='relu'))            
-model.add(Dense(20, activation='relu'))               
-model.add(Dense(7, activation='softmax'))          
-                                                               
+model.add(Dense(100, activation='linear', input_dim=30))    # 기본 activation의 default 값은 'linear'
+model.add(Dropout(0.3))                                     # 연산 때 30% 비율로 랜덤하게 노드를 없앰
+model.add(Dense(100, activation='relu'))
+model.add(Dropout(0.2))                                     # 연산 때 20% 비율로 랜덤하게 노드를 없앰
+model.add(Dense(100, activation='relu'))        # relu 는 히든레이어에서만 사용가능함
+model.add(Dropout(0.2))                                     # 연산 때 20% 비율로 랜덤하게 노드를 없앰
+model.add(Dense(100, activation='relu'))
+model.add(Dense(1, activation = 'sigmoid'))     # output에서 activatoin = 'sigmoid' ==> 마지막 결과값이 0~1사이로 나옴
+                                                 # **** 이진 분류모델의 경우 반올림하여 0과 1로 결과값을 받음
 
-#3. 컴파일 훈련
+#3. 훈련
+model.compile(loss='binary_crossentropy', optimizer='adam',
+            #   metrics=['accuracy'],
+              metrics=['accuracy', 'mse'])  # 이진 분류함수의 경우 loss = 'binary_crossentropy' 이고 
+                                            # 평가지표 metrics['accuracy']를 사용, 회귀모델의 경우 mse를 사용함
 
-model.compile(loss='categorical_crossentropy', optimizer='adam', # 다중 분류에서는 로스함수를 'categorical_crossentropy' 로 써준다 (99퍼센트로)
-              metrics=['accuracy'])
+import datetime
+date = datetime.datetime.now()      # 2022-07-07 17:21:42.275191
+date = date.strftime("%m%d_%H%M")   # 0707_1723
+print(date)
 
-es = EarlyStopping(monitor='val_loss', patience=50, mode='auto', verbose=1, 
-                              restore_best_weights=True)   
+filepath = './_ModelCheckPoint/k26/'
+filename = '{epoch:04d}-{val_loss:.4f}.hdf5'
 
-model.fit(x_train, y_train, epochs=50, batch_size=5,
+earlyStopping = EarlyStopping(monitor = 'val_loss', patience=100, mode='min', verbose=1, 
+                              restore_best_weights=True)
+mcp = ModelCheckpoint(monitor='val_loss', mode='auto', verbose=1, 
+                      save_best_only=True, 
+                      filepath="".join([filepath, '04_', date, '_', filename])
+                      )
+start_time = time.time() 
+hist = model.fit(x_train, y_train, epochs=1000, batch_size=100,
                  validation_split=0.2,
-                 callbacks=[es],
+                 callbacks=[earlyStopping, mcp],
                  verbose=1)
+end_time = time.time() - start_time
+
 
 #4. 평가, 예측
-# loss, acc= model.evaluate(x_test, y_test)
-# print('loss : ', loss)
-# print('accuracy : ', acc)
+loss = model.evaluate(x_test, y_test)
+print('loss : ', loss)
 
-results= model.evaluate(x_test, y_test)
-print('loss : ', results[0])
-print('accuracy : ', results[1])
-
-
+#=== 과제 1. accuracy_score 완성하기 =========================================
 y_predict = model.predict(x_test)
+
+y_predict = y_predict.flatten()                 # 차원 펴주기
+y_predict = np.where(y_predict > 0.5, 1 , 0)   # 0.5보다 크면 1, 작으면 0
 print(y_predict)
-print(y_test)
-y_predict = np.argmax(y_predict, axis= 1)  # 판다스 겟더미 쓸때는 tf.argmax sklearn 원핫인코딩 쓸때는 np
-print(y_predict)
-y_test = np.argmax(y_test, axis= 1)
-print(y_test)
-# y_predict = to_categorical(y_predict)
-# y_test = np.argmax(y_test, axis= 1)
-print(np.unique(y_predict))
-print(np.unique(y_test))
+
+from sklearn.metrics import classification_report
+print(classification_report(y_test, y_predict))
+# ====================================================== 과제 1 끝 ===========
+
+acc = accuracy_score(y_test, y_predict)
+print("=====================================================================")   
+print('acc 스코어 : ', acc)  
 
 
+#================================= 1. 기본 출력 ===================================#
+# loss :  [0.07133284211158752, 0.019666554406285286]
+# acc 스코어 :  0.9707602339181286
+# k24_0707_1915_0019-0.0723.hdf5
+#=================================================================================#
 
-acc= accuracy_score(y_test, y_predict)
-print('acc스코어 : ', acc) 
-
-#drop out 이전
-# loss :  0.9041478037834167
-# accuracy :  0.6589177250862122
-
-#drop out 이후
+#================================ 2. dorpout 적용 ================================#
+# loss :  [0.16834446787834167, 0.030178742483258247]
+# acc 스코어 :  0.9590643274853801
+# 4_0708_1100_0095-0.0679.hdf5
+#=================================================================================#
