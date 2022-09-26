82d9461c118d1c7327ad5c5a7967c6f27187d861
junginee <107666154+junginee@users.noreply.github.com>
2022-09-26 18:31:31 +0900

    Update keras25_MCP_11_kaggle_house.py

--- keras/keras25_MCP_11_kaggle_house.py
+++ keras/keras25_MCP_11_kaggle_house.py
@@ -1,6 +1,3 @@
-
-
-
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
@@ -20,25 +17,25 @@ print(a)
 print(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))) #현재 파일이 위치한 경로
 print(a.split("\\")[-1]) #현재 파일 명
 current_name = a.split("\\")[-1]
-##########################밑에 filepath경로에 추가로  + current_name + '/' 삽입해야 돌아감#######################
+##########################밑에 filepath경로에 추가로  + current_name + '/' 삽입해야 돌아감##################
 
 #1. 데이터
-path = './_data/kaggle_house/' # 경로 = .현재폴더 /하단
-train_set = pd.read_csv(path + 'train.csv', # train.csv 의 데이터가 train set에 들어가게 됨
-                        index_col=0) # 0번째 컬럼은 인덱스로 지정하는 명령
+path = './_data/kaggle_house/' 
+train_set = pd.read_csv(path + 'train.csv', 
+                        index_col=0) 
 #print(train_set)
-#print(train_set.shape) # (1460, 80) 원래 열이 81개지만, id를 인덱스로 제외하여 80개
+#print(train_set.shape) 
 
 test_set = pd.read_csv(path + 'test.csv',
                        index_col=0)
 
-drop_cols = ['Alley', 'PoolQC', 'Fence', 'MiscFeature'] # Columns with more than 70% of missing values
+drop_cols = ['Alley', 'PoolQC', 'Fence', 'MiscFeature']
 test_set.drop(drop_cols, axis = 1, inplace =True)
 
 sample_submission = pd.read_csv(path + 'sample_submission.csv',
                        index_col=0)
 #print(test_set)
-#print(test_set.shape) # (1459, 79) # 예측 과정에서 쓰일 예정
+#print(test_set.shape) # (1459, 79) 
 
 train_set.drop(drop_cols, axis = 1, inplace =True)
 cols = ['MSZoning', 'Street','LandContour','Neighborhood','Condition1','Condition2',
@@ -53,19 +50,14 @@ for col in tqdm_notebook(cols):
     train_set[col]=le.fit_transform(train_set[col])
     test_set[col]=le.fit_transform(test_set[col])
 
-#print(train_set.columns)
-#print(train_set.info()) # 각 컬럼에 대한 디테일한 내용 출력 / null값(중간에 빠진 값) '결측치'
-#print(train_set.describe())
 
 #### 결측치 처리 1. 제거 ####
-print(train_set.isnull().sum()) # 각 컬럼당 null의 갯수 확인가능
-train_set = train_set.fillna(train_set.mean()) # nan 값을 채우거나(fillna) 행별로 모두 삭제(dropna)
+print(train_set.isnull().sum()) 
+train_set = train_set.fillna(train_set.mean())
 print(train_set.isnull().sum())
-print(train_set.shape) # (1460, 80) 데이터가 얼마나 삭제된 것인지 확인가능(1460-1460=0)
- 
+print(train_set.shape)  
 
 test_set = test_set.fillna(test_set.mean())
-
 
 x = train_set.drop(['SalePrice'], axis=1) # axis는 'count'가 컬럼이라는 것을 명시하기 위해
 print(x)
@@ -76,11 +68,8 @@ y = train_set['SalePrice']
 print(y)
 print(y.shape) # (1460, )
 
-
-
 x_train, x_test, y_train, y_test = train_test_split(x, y,
         train_size=0.75, shuffle=True, random_state=68)
-
 
 scaler = MinMaxScaler()
 scaler.fit(x_train)
@@ -134,7 +123,6 @@ hist = model.fit(x_train, y_train, epochs=500, batch_size=10,
 end_time = time.time() 
 
 
-
 #4. 평가 예측
 print("걸린시간 : ", end_time)
 
@@ -143,11 +131,10 @@ print('loss :', loss)
 
 y_predict = model.predict(x_test)
 
-def RMSE(y_test, y_predict) : #(원y값, 예측y값)
-    return np.sqrt(mean_squared_error(y_test, y_predict)) # MSE에 루트를 씌워 돌려주겠다.
+def RMSE(y_test, y_predict) :
+    return np.sqrt(mean_squared_error(y_test, y_predict))
 
 rmse = RMSE(y_test, y_predict)
 print("RMSE : ", rmse)
 
 y_summit = model.predict(test_set)
-
