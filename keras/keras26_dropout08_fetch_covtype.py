79dc0fe325da479673b610786d830263ff791024
junginee <107666154+junginee@users.noreply.github.com>
2022-09-26 18:47:08 +0900

    Update keras26_dropout08_fetch_covtype.py

--- keras/keras26_dropout08_fetch_covtype.py
+++ keras/keras26_dropout08_fetch_covtype.py
@@ -1,99 +1,81 @@
-import numpy as np
-from sklearn import datasets
+import numpy as np 
 from sklearn.datasets import fetch_covtype
+from tensorflow.python.keras.models import Sequential,Model,load_model
+from tensorflow.python.keras.layers import Dense,Input,Dropout
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler, StandardScaler, MaxAbsScaler, RobustScaler
-from tensorflow.python.keras.models import Sequential, Model
-from tensorflow.python.keras.layers import Dense, Input
-from tensorflow.python.keras.callbacks import EarlyStopping, ModelCheckpoint
-from sklearn.metrics import r2_score, accuracy_score
-import time
-
-
-#1. 데이터
-datasets = fetch_covtype()
-x = datasets.data
-y = datasets.target
-
-# One Hot Encoding 
-from sklearn.preprocessing import OneHotEncoder
-onehot_encoder = OneHotEncoder(categories='auto', sparse=False)
-y = y.reshape(-1, 1)
-onehot_encoder.fit(y)
-y = onehot_encoder.transform(y)
-
-x_train, x_test, y_train, y_test = train_test_split(
-    x, y, train_size=0.7, random_state=66
-)
-
-
-scaler = RobustScaler()
-scaler.fit(x_train)
-x_train = scaler.transform(x_train)
-x_test = scaler.transform(x_test)
-
-
-#2. 모델 구성
-model = Sequential()
-model.add(Dense(100, activation='linear', input_dim=54))
-model.add(Dense(100, activation='relu'))
-model.add(Dense(100, activation='relu'))
-model.add(Dense(100, activation='relu'))
-model.add(Dense(7, activation='softmax'))
-
-
-#3. 훈련
-model.compile(loss='categorical_crossentropy', optimizer='adam',    # 다중분류에서 loss = 'categorical_crossentropy'를 사용함
-              metrics=['accuracy'])   
-
+from sklearn.metrics import r2_score,accuracy_score
+from tensorflow.python.keras.callbacks import EarlyStopping,ModelCheckpoint
+import tensorflow as tf
+from sklearn.preprocessing import MinMaxScaler,StandardScaler,MaxAbsScaler,RobustScaler
 import datetime
-date = datetime.datetime.now()      
-date = date.strftime("%m%d_%H%M")   
+import pandas as pd
 
-filepath = './_ModelCheckPoint/k24/'
-filename = '{epoch:04d}-{val_loss:.4f}.hdf5'
+#1.데이터 
+datasets = fetch_covtype()
+x= datasets.data
+y= datasets.target
 
-earlyStopping = EarlyStopping(monitor = 'val_loss', patience=50, mode='min', verbose=1, 
-                              restore_best_weights=True)
+print(x.shape, y.shape) #(581012, 54) (581012, )
+print(np.unique(y,return_counts=True)) #(array[1 2 3 4 5 6 7],array[211840, 283301,  35754,   2747,   9493,  17367,  20510] )
 
-mcp = ModelCheckpoint(monitor='val_loss', mode='auto', verbose=1, 
-                      save_best_only=True, 
-                      filepath="".join([filepath, '14_', date, '_', filename])
-                      )
+#사이킷런
+from sklearn.preprocessing import OneHotEncoder
+one = OneHotEncoder(categories='auto',sparse= False)
+y = y.reshape(-1,1)
+one.fit(y)
+y = one.transform(y)
 
-start_time = time.time()
-hist = model.fit(x_train, y_train, epochs=5000, batch_size=128, 
-                 validation_split=0.2,
-                #  validation_data=(x_val,y_val),
-                 callbacks=[earlyStopping, mcp],
-                 verbose=1)
-end_time = time.time() - start_time
+print(y)
+print(y.shape)
 
-#4. 평가, 예측
-loss, acc = model.evaluate(x_test, y_test)
-print('loss : ', loss)       
-print('accuracy : ', acc)
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.3,shuffle=True,
+                                                     random_state=58)
+
+# minmax , standard
+scaler = RobustScaler()
+x_train = scaler.fit_transform(x_train)#스케일링한것을 보여준다.
+x_test = scaler.transform(x_test)#test는 transfrom만 해야됨 
+
+#2.모델
+model = Sequential()
+model.add(Dense(40, input_dim=54))
+model.add(Dense(30,activation ='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(30,activation ='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(30,activation ='relu'))
+model.add(Dense(30,activation ='relu'))
+model.add(Dense(7,activation ='softmax')) #소프트맥스는 모든 연산값의 합이 1.0,그중 가장 큰값(퍼센트)을 선택,so 마지막 노드3개* y의 라벨의 갯수
+#softmax는 아웃풋만 가능 히든에서 x
+date = datetime.datetime.now()
+date = date.strftime('%m%d_%H%M')
+
+#3.컴파일,훈련
+model.compile(loss= 'categorical_crossentropy', optimizer ='adam', metrics='accuracy') #다중분류는 무조건 loss에 categorical_crossentropy
+ #분류모델에서 셔플 중요! ,false로 하면 순차적으로 나와서 2가 아예 안나옴.
+
+earlyStopping= EarlyStopping(monitor='val_loss',patience=10,mode='min',restore_best_weights=True,verbose=1)
+
+# filepath = './_ModelCheckpoint/k24/'
+# filename = '{epoch:04d}-{val_loss:.4f}.hdf5'
+
+# mcp = ModelCheckpoint(monitor='val_loss',mode='auto',save_best_only=True,verbose=1,
+#                       filepath="".join([filepath,'fetch_covtype',date,'_',filename]))
+
+model.fit(x_train, y_train, epochs=100, batch_size=100,validation_split=0.2,callbacks=[earlyStopping,],verbose=1) #batch default :32
+
+
+#4.평가,예측
+
+results = model.evaluate(x_test,y_test)
+print('loss : ', results[0])
 
 y_predict = model.predict(x_test)
-y_predict = y_predict.argmax(axis=1)      # tensorflow에서 사용 : to_categorical
-y_test = y_test.argmax(axis=1)            # tensorflow에서 사용 : to_categorical
-# y_predict = tf.argmax(y_predict, axis=1)    # pandas에서 사용 : get_dummies
-# y_test = tf.argmax(y_test, axis=1)          # pandas에서 사용 : get_dummies
+y_predict = tf.argmax(y_predict,axis=1) 
 
+y_test = tf.argmax(y_test,axis=1) 
+acc = accuracy_score(y_test,y_predict)
+print('acc : ',acc)
 
-
-print("================================ y_predict =================================")
-print(y_predict)
-print(y_test)
-print("============================================================================")
-
-acc = accuracy_score(y_test, y_predict)
-print("============================================================================")   
-print('acc 스코어 : ', acc)  
-
-
-#================================= 1. 기본 출력 ===================================#
-# loss :  0.12761995196342468
-# accuracy :  0.979629635810852
-# 07_0707_1954_0033-0.1017.hdf5
-#=================================================================================#
+# loss :  0.4642634391784668
+# acc :  0.8038541857903433
