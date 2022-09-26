8b0afb13c7388778bb238237cee0353943dbae44
junginee <107666154+junginee@users.noreply.github.com>
2022-09-26 18:43:59 +0900

    Update keras26_dropout09_Ddarung.py

--- keras/keras26_dropout09_Ddarung.py
+++ keras/keras26_dropout09_Ddarung.py
@@ -1,156 +1,89 @@
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
-from sklearn.preprocessing import MaxAbsScaler, RobustScaler
 import numpy as np
 import pandas as pd
-from sqlalchemy import true #pandas : 엑셀땡겨올때 씀
-from tensorflow.python.keras.models import Sequential, Model
-from tensorflow.python.keras.layers import Dense, Input
+from sklearn import datasets
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score, mean_squared_error
-
-###########################폴더 생성시 현재 파일명으로 자동생성###########################################
-import inspect, os
-a = inspect.getfile(inspect.currentframe()) #현재 파일이 위치한 경로 + 현재 파일 명
-print(a)
-print(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))) #현재 파일이 위치한 경로
-print(a.split("\\")[-1]) #현재 파일 명
-current_name = a.split("\\")[-1]
-##########################밑에 filepath경로에 추가로  + current_name + '/' 삽입해야 돌아감#######################
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, MaxAbsScaler, RobustScaler
+from tensorflow.python.keras.models import Sequential, Model, load_model
+from tensorflow.python.keras.layers import Dense, Input
+from tensorflow.python.keras.callbacks import EarlyStopping, ModelCheckpoint
+from sklearn.metrics import r2_score, accuracy_score, mean_squared_error
+import time
 
 
 #1. 데이터
 path = './_data/ddarung/'
-train_set = pd.read_csv(path + 'train.csv', # + 명령어는 문자를 앞문자와 더해줌
-                        index_col=0) # index_col=n n번째 컬럼을 인덱스로 인식
-print(train_set)
-print(train_set.shape) # (1459, 10)
-
-test_set = pd.read_csv(path + 'test.csv', # 예측에서 쓸거임                
+train_set = pd.read_csv(path + 'train.csv', 
+                        index_col=0)
+test_set = pd.read_csv(path + 'test.csv',   # 예측에서 사용!!
                        index_col=0)
-print(test_set)
-print(test_set.shape) # (715, 9)
 
-print(train_set.columns)
-print(train_set.info()) # info 정보출력
-print(train_set.describe()) # describe 평균치, 중간값, 최소값 등등 출력
+#### 결측치 처리 ####
+test_set = test_set.fillna(method='ffill')
+train_set = train_set.dropna()  # nan 값 삭제
 
-#### 결측치 처리 1. 제거 ####
-print(train_set.isnull().sum())
-train_set = train_set.fillna(train_set.mean()) # dropna() : train_set 에서 na, null 값 들어간 행 삭제
-test_set = test_set.fillna(test_set.mean()) # test_set 에서 이빨빠진데 바로  ffill : 위에서 가져오기 test_set.mean : 평균값
-print(train_set.isnull().sum()) 
-print(train_set.shape) # (1328, 10)
+x = train_set.drop(['count'], axis=1)
+y = train_set['count']
 
-############################
-
-
-x = train_set.drop(['count'], axis=1)  # drop 데이터에서 ''사이 값 빼기
-print(x)
-print(x.columns)
-print(x.shape) # (1459, 9)
-
-y = train_set['count'] 
-print(y)
-print(y.shape) # (1459,)
-
-x_train, x_test, y_train, y_test = train_test_split(x,y,
-                                                    train_size=0.75,
-                                                    random_state=31
-                                                    )
-
-# scaler = MinMaxScaler()
-# scaler = StandardScaler()
-scaler = MaxAbsScaler()
-# scaler = RobustScaler()
-scaler.fit(x_train)
-x_train = scaler.transform(x_train)
-x_test = scaler.transform(x_test)
-test_set = scaler.transform(test_set)
-print(np.min(x_train))  # 0.0
-print(np.max(x_train))  # 1.0
-
-print(np.min(x_test))  # 1.0
-print(np.max(x_test))  # 1.0
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, train_size=0.7, random_state=66
+)
 
 
 #2. 모델구성
+model = Sequential()
+model.add(Dense(100, activation = 'linear', input_dim=9))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(1, activation='linear'))
 
-# model = load_model("./_save/keras22_hamsu09_ddarung.h5")
-# model = Sequential()
-# model.add(Dense(100, activation='selu', input_dim=9))
-# model.add(Dense(100, activation='selu'))
-# model.add(Dense(100, activation='selu'))
-# model.add(Dense(1))
 
-input1 = Input(shape=(9,))
-dense1 = Dense(100, activation='selu')(input1)
-dense2 = Dense(100, activation='selu')(dense1)
-dense3 = Dense(100, activation='selu')(dense2)
-output1 = Dense(1)(dense3)
-model = Model(inputs=input1, outputs=output1)     
 
 #3. 컴파일, 훈련
+model.compile(loss = 'mae', optimizer="adam", metrics=['mse'])
 
-model.compile(loss='mse', optimizer='adam', metrics=['mae'])
-from tensorflow.python.keras.callbacks import EarlyStopping, ModelCheckpoint
 import datetime
-date = datetime.datetime.now()
-date = date.strftime("%m%d_%H%M") # 0707_1723
+date = datetime.datetime.now()      # 2022-07-07 17:21:42.275191
+date = date.strftime("%m%d_%H%M")   # 0707_1723
 print(date)
 
-
-filepath = './_ModelCheckPoint/' + current_name + '/'
+filepath = './_ModelCheckPoint/k24/'
 filename = '{epoch:04d}-{val_loss:.4f}.hdf5'
 
-earlyStopping = EarlyStopping(monitor='val_loss', patience=100, mode='auto', verbose=1, 
-                              restore_best_weights=True)        
+earlyStopping = EarlyStopping(monitor='val_loss', patience=200, mode='min',
+                              restore_best_weights=True,
+                              verbose=1)
+mcp = ModelCheckpoint(monitor='val_loss', mode='auto', verbose=1, 
+                      save_best_only=True, 
+                      filepath="".join([filepath, '09_', date, '_', filename])
+                      )
 
-#mcp = ModelCheckpoint(monitor='val_loss', mode='auto', verbose=1, save_best_only=True, 
-#                      filepath= "".join([filepath, date, '_', filename])
-
-hist = model.fit(x_train, y_train, epochs=1000, batch_size=100,
-                 validation_split=0.2,
-                 callbacks=[earlyStopping, mcp],
-                 verbose=1)
-
-#4. 평가, 예측
-loss = model.evaluate(x, y) 
+start_time = time.time()
+hist = model.fit(x_train, y_train, epochs=5000, batch_size=100, 
+          validation_split=0.2,
+          callbacks=[earlyStopping, mcp],
+          verbose=1)
+end_time = time.time() - start_time
 
 
+#4. 평가예측
+loss = model.evaluate(x_test, y_test)
+print('loss : ', loss)  
+
+#### mse를 rmse로 변환 ####
 y_predict = model.predict(x_test)
-
-def RMSE(a, b): 
-    return np.sqrt(mean_squared_error(a, b))
+def RMSE(y_test, y_predict):
+    return np.sqrt(mean_squared_error(y_test, y_predict))
 
 rmse = RMSE(y_test, y_predict)
-
-
-from sklearn.metrics import r2_score
-r2 = r2_score(y_test, y_predict)
-
-print('loss : ', loss)
 print("RMSE : ", rmse)
-print('r2스코어 : ', r2)
 
-# y_summit = model.predict(test_set)
-
-# print(y_summit)
-# print(y_summit.shape) # (715, 1)
-
-# submission_set = pd.read_csv(path + 'submission.csv', # + 명령어는 문자를 앞문자와 더해줌
-#                              index_col=0) # index_col=n n번째 컬럼을 인덱스로 인식
-
-# print(submission_set)
-
-# submission_set['count'] = y_summit
-# print(submission_set)
+r2 = r2_score(y_test, y_predict)
+print("R2 : ", r2)
 
 
-# submission_set.to_csv('./_data/ddarung/submission_maxabs.csv', index = True)
-
-
-#drop out 이전
-
-
-
-#drop out 이후
+#================================= 1. 기본 출력 ===================================#
+# loss :  [40.44511795043945, 3217.836669921875]
+# R2 :  0.4822973526333515
+# 09_0707_2004_0415-36.6455.hdf5
+#=================================================================================#
