2b0ea1b24316bdb39725f6fb1d091fc89b637eb8
junginee <107666154+junginee@users.noreply.github.com>
2022-09-26 18:57:17 +0900

    Update keras29_Fashion_mnist_imshow.py

--- keras/keras29_Fashion_mnist_imshow.py
+++ keras/keras29_Fashion_mnist_imshow.py
@@ -6,7 +6,7 @@ from tensorflow.keras.datasets import fashion_mnist
 (x_train, y_train),(x_test, y_test) = fashion_mnist.load_data()
 
 print(x_train.shape, y_train.shape) 
-print(x_test.shape, y_test.shape) 
+print(x_test.shape, y_test.shape)   
 
 print(x_train[0])
 print(y_train[0])
