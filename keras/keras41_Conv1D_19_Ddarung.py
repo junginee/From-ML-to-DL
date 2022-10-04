066b257b989057bec7e1ebc42f23aba34603d086
junginee <107666154+junginee@users.noreply.github.com>
2022-10-04 16:39:23 +0900

    Update keras41_Conv1D_19_Ddarung.py

---
+++ keras/keras41_Conv1D_19_Ddarung.py
@@ -0,0 +1,69 @@
+import numpy as np 
+import pandas as pd # csv 파일 당겨올 때 사용
+from tensorflow.python.keras.models import Sequential,Model,load_model
+from tensorflow.python.keras.layers import Dense,Dropout,Input,LSTM,Conv1D,Flatten
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_squared_error 
+from tensorflow.python.keras.callbacks import EarlyStopping,ModelCheckpoint
+from sklearn.preprocessing import MinMaxScaler,StandardScaler,MaxAbsScaler,RobustScaler
+
+
+#1.데이터 
+path = './_data/ddarung/'
+train_set = pd.read_csv(path + 'train.csv', index_col=0) 
+
+print(train_set)
+print(train_set.shape) # (1459, 10)
+
+test_set = pd.read_csv(path + 'test.csv', index_col=0) 
+
+train_set = train_set.dropna()
+test_set= test_set.fillna(test_set.mean())
+
+x = train_set.drop(['count'], axis=1,)
+y = train_set['count']
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, 
+                                                    train_size=0.7,
+                                                    shuffle=True,
+                                                    random_state=48)
+
+scaler = MaxAbsScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+
+print(x_train.shape,x_test.shape)
+x_train =x_train.reshape(929,9,1)
+x_test = x_test.reshape(399,9,1)
+
+#2.모델구성
+model = Sequential()
+model.add(Conv1D(200,2,activation='relu', input_shape=(9,1))) 
+model.add(Flatten())
+model.add(Dense(100,activation= 'relu'))
+model.add(Dense(100,activation= 'relu'))
+model.add(Dense(100,activation= 'relu'))
+model.add(Dense(100,activation= 'relu'))
+model.add(Dense(1))
+
+#3. 컴파일, 훈련
+model.compile(loss= 'mse', optimizer ='adam')
+
+earlyStopping =EarlyStopping(monitor = 'val_loss',patience=10,mode='min',restore_best_weights=True,verbose=1)
+
+model.fit(x_train, y_train, epochs=200, batch_size=100,validation_split=0.2,callbacks=[earlyStopping],verbose=2)
+
+#4.평가,예측
+loss = model.evaluate(x_test, y_test)
+print('loss: ', loss)
+
+y_predict = model.predict(x_test)
+
+def RMSE(y_test, y_predict):
+    return np.sqrt(mean_squared_error(y_test, y_predict)) 
+
+rmse = RMSE(y_test, y_predict)
+print("RMSE : ", rmse)
+
+# loss:  2051.07177734375
+# RMSE :  45.288760139035766
