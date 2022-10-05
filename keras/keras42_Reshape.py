027b7bb6849e098e245f9de2dbcd3b7a35f9060c
junginee <107666154+junginee@users.noreply.github.com>
2022-10-05 17:10:35 +0900

    Update keras42_Reshape.py

--- keras/keras42_Reshape.py
+++ keras/keras42_Reshape.py
@@ -8,7 +8,6 @@ from tensorflow.keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 from sklearn.utils import validation
 
-
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 y_train = to_categorical(y_train)
@@ -16,19 +15,16 @@ y_test = to_categorical(y_test)
 print(x_train.shape, y_train.shape)    
 print(x_test.shape, y_test.shape)      
 
-x_train = x_train.reshape(60000, 28, 28, 1) 
 
-x_test = x_test.reshape(10000, 28, 28, 1)                            
+x_train = x_train.reshape(60000, 28, 28, 1) 
+x_test = x_test.reshape(10000, 28, 28, 1)                          
                                          
 
 print(np.unique(y_train, return_counts=True))   #10개 
-
-
 print(x_train.shape, y_train.shape) #(60000, 28, 28, 1) (60000,)
 
 
 # 2. 모델구성
-
 model  =  Sequential() 
 model.add(Conv2D(64, kernel_size=(3,3), padding = 'same', input_shape=(28, 28, 1 ) ))
 model.add(MaxPooling2D())                                     #(None, 14, 14, 64) 
