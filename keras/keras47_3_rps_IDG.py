c6755c93ae6e693c6ab8295314a55997d5ae94e5
junginee <107666154+junginee@users.noreply.github.com>
2022-10-07 09:53:58 +0900

    Update keras47_3_rps_IDG.py

--- keras/keras47_3_rps_IDG.py
+++ keras/keras47_3_rps_IDG.py
@@ -1,5 +1,3 @@
-
-
 import numpy as np
 from keras.preprocessing.image import ImageDataGenerator
 import warnings
@@ -10,11 +8,7 @@ datagen = ImageDataGenerator(
     rescale=1./255 #원본 영상은 0-255의 RGB 계수로 구성되는데, 이 같은 입력값은 모델을 효과적으로 학습시키기에 너무 높다. 
                     #그래서 이를 1/255로 스케일링하여 0-1 범위로 변환 /  이는 다른 전처리 과정에 앞서 가장 먼저 적용
            
-    )
-
-
-
-    
+    )  
 
 
 data = datagen.flow_from_directory(
@@ -26,13 +20,8 @@ data = datagen.flow_from_directory(
     shuffle=True
 ) #Found 2520 images belonging to 3 classes.
 
-
 print(data[0][0].shape) #(2520, 100, 100, 3)
 print(data[0][1].shape) #(2520, 3)
 
-
-
 np.save('d:/study_data/_save/_npy/keras47_3_x.npy', arr=data[0][0])
 np.save('d:/study_data/_save/_npy/keras47_3_y.npy', arr=data[0][1])
-
-
