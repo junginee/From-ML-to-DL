c1ac3fcd5aa4810b0a454babdcf275cf8691166e
junginee <107666154+junginee@users.noreply.github.com>
2022-10-17 00:03:59 +0900

    Update keras01.py

--- keras/keras01.py
+++ keras/keras01.py
@@ -6,7 +6,7 @@ y = np.array([1,2,3])
                                                                                                                         
 #2.모델구성                                                                                                                                      
 from tensorflow.keras.models import Sequential         
-from tensorflow.keras.layers import Dense                            
+from tensorflow.keras.layers import Dense                                        
                                                                       
 model = Sequential()                                      
 model.add(Dense(4, input_dim=1))   
