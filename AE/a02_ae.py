dcd150af17a9094940399b896d951dbe9e2ea878
junginee <107666154+junginee@users.noreply.github.com>
2022-10-16 13:35:00 +0900

    Update a02_ae.py

--- AE/a02_ae.py
+++ AE/a02_ae.py
@@ -1,7 +1,7 @@
 import numpy as np        
 from keras.datasets import mnist         
                                                                     
-(x_train, _), (x_test, _) = mnist.load_data()   
+(x_train, _), (x_test, _) = mnist.load_data()       
 
 x_train = x_train.reshape(60000, 784).astype('float32')/255.             
 x_test = x_test.reshape(10000, 784).astype('float32')/255.
