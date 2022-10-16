17768dc2d697ee5b469ac40df9e84bfa5a05e07a
junginee <107666154+junginee@users.noreply.github.com>
2022-10-16 13:31:14 +0900

    Update keras01.py

--- keras/keras01.py
+++ keras/keras01.py
@@ -1,6 +1,6 @@
       
 #1.데이터
-import numpy as np  
+import numpy as np            
 x = np.array([1,2,3])       
 y = np.array([1,2,3])                                                                                                                                                                          
                                                                                                                         
